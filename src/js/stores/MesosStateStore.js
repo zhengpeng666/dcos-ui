3a4e7fc395986f030bbe978cc2a864d6ee0cecae
Felix Gertz <felix@mesosphere.io>
2015-02-23 14:11:02 -0800

    Stringfilter option with improved RegExp.

--- src/js/stores/MesosStateStore.js
+++ src/js/stores/MesosStateStore.js
@@ -90,25 +90,28 @@ var MesosStateStore = _.extend({}, EventEmitter.prototype, {
     });
   },
 
+  applySearchString: function (frameworks, searchString) {
+    var searchPattern = new RegExp(searchString, "i");
+    var valuesPattern = /:\"[^\"]+\"/g;
+    var cleanupPattern = /[:\"]/g;
+
+    return _.filter(frameworks, function (framework) {
+      var str = JSON.stringify(framework)
+        .match(valuesPattern)
+        .join(" ")
+        .replace(cleanupPattern, "");
+      return searchPattern.test(str);
+    });
+  },
+
   applyAllFilter: function () {
     _mesosStateFiltered = _.clone(_mesosState);
 
     if (_filterOptions.searchString !== "") {
-      (function () {
-        var searchPattern = new RegExp(_filterOptions.searchString, "i");
-        var nonStringValuesPattern = /:(?:[^\"]+),/g;
-        var keysAndBracketsPattern = /\"(?:\w+)\":|[{}\[\]]|\"/g;
-        var commaPattern = /,/g;
-
-        _mesosStateFiltered.frameworks = _.filter(
-            _mesosStateFiltered.frameworks, function (framework) {
-          return searchPattern.test(JSON.stringify(framework).
-            replace(nonStringValuesPattern, ":").
-            replace(keysAndBracketsPattern, "").
-            replace(commaPattern, " ")
-          );
-        });
-      })();
+      _mesosStateFiltered.frameworks = this.applySearchString(
+        _mesosState.frameworks,
+        _filterOptions.searchString
+      );
     }
   },
 
