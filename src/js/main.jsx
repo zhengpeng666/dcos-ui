42eb5ae23a9043853830fe9b7af5daf950641db3
mlunoe <michael.lunoe@gmail.com>
2015-02-23 08:16:36 -0800

    Hack for getting React to understand clip-path attr

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 
 var React = require("react");
+var DOMProperty = require("react/lib/DOMProperty");
 var Router = require("react-router");
 var Route = Router.Route;
 
@@ -8,6 +9,13 @@ require("./scrollHandler");
 var ActivityPage = require("./components/ActivityPage");
 var Index = require("./components/Index");
 var ServicesPage = require("./components/ServicesPage");
+
+// hack for getting react to render clip-path attribute
+DOMProperty.injection.injectDOMPropertyConfig({
+  isCustomAttribute: function (attributeName) {
+    return (attributeName === "clip-path");
+  }
+});
 
 /* jshint trailing:false, quotmark:false, newcap:false */
 /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
