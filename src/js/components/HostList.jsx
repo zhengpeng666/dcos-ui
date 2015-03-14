5f683d69b5839a86fdcd2f128fb609556d40f101
mlunoe <michael.lunoe@gmail.com>
2015-03-14 16:29:32 -0700

    Introducing sortable table component

--- src/js/components/HostList.jsx
+++ src/js/components/HostList.jsx
@@ -1,9 +1,87 @@
 /** @jsx React.DOM */
 
 var _ = require("underscore");
+var Humanize = require("humanize");
 var React = require("react/addons");
 
-var HostItem = require("./HostItem");
+var Table = require("./Table");
+
+function renderHeadline(prop) {
+  return (
+    <h5 className="flush-top flush-bottom">
+      <i className="icon icon-small icon-small-white border-radius"></i>
+      {prop}
+    </h5>
+  );
+}
+
+function renderTask(prop, model) {
+  return (
+    <span>
+      {model[prop]}
+      <span className="mobile-displayed-text"> Tasks</span>
+    </span>
+  );
+}
+
+function renderStats(prop, model) {
+  var value = _.last(model.used_resources[prop]).value;
+  if(prop !== "cpus") {
+    value = Humanize.filesize(value * 1024 * 1024, 1024, 1);
+  }
+
+  return (
+    <span>
+      {value}
+    </span>
+  );
+}
+
+function classNameStats(prop, row, sortBy) {
+  var className = "align-right mobile-hidden";
+  if (sortBy.prop === prop) {
+    className += " highlighted";
+  }
+
+  return className;
+}
+
+var columns = [
+  {
+    prop: "hostname",
+    render: renderHeadline,
+    sortable: true,
+    title: "SERIVCE NAME"
+  },
+  {
+    className: "align-right",
+    prop: "tasks_size",
+    render: renderTask,
+    sortable: true,
+    title: "TASKS"
+  },
+  {
+    className: classNameStats,
+    prop: "cpus",
+    render: renderStats,
+    sortable: true,
+    title: "CPU"
+  },
+  {
+    className: classNameStats,
+    prop: "mem",
+    render: renderStats,
+    sortable: true,
+    title: "MEM"
+  },
+  {
+    className: classNameStats,
+    prop: "disk",
+    render: renderStats,
+    sortable: true,
+    title: "DISK"
+  },
+];
 
 var HostList = React.createClass({
 
@@ -19,148 +97,32 @@ var HostList = React.createClass({
     };
   },
 
-  getInitialState: function() {
-    return {
-      sortKey: null,
-      sortOrder: null
-    };
-  },
+  sortFunction: function (prop) {
+    if (prop === "cpus" || prop === "mem" || prop === "disk") {
 
-  sortBy: function (comparator) {
-    var state = this.state;
-    var sortOrder;
-
-    if (state.sortKey !== comparator) {
-      sortOrder = 1;
-    }
-
-    if (state.sortKey == null || state.sortKey === comparator) {
-      if (state.sortOrder == null) {
-        sortOrder = 1;
-      } else if (state.sortOrder === 1) {
-        sortOrder = -1;
-      } else if (state.sortOrder === -1) {
-        sortOrder = null;
-        comparator = null;
-      }
-    }
-
-    this.setState({
-      sortKey: comparator,
-      sortOrder: sortOrder
-    });
-  },
-
-  sortHosts: function (hosts) {
-    var key = this.state.sortKey;
-    var order = this.state.sortOrder;
-    var sortfunction = function (a, b) {
-      if (a[key] > b[key]) {
-        return 1 * order;
-      }
-      if (a[key] < b[key]) {
-        return -1 * order;
-      }
-      return 0;
-    };
-
-    if (key === "cpus" || key === "mem" || key === "disk") {
-      sortfunction = function (a, b) {
-        a = _.last(a.used_resources[key]).percentage;
-        b = _.last(b.used_resources[key]).percentage;
-        if (a > b) {
-          return 1 * order;
-        }
-        if (a < b) {
-          return -1 * order;
-        }
-        return 0;
+      return function (a, b) {
+        a = _.last(a.used_resources[prop]).value;
+        b = _.last(b.used_resources[prop]).value;
+        return a < b ? -1 : a > b ? 1 : 0;
       };
     }
 
-    if (sortfunction != null) {
-      return _.clone(hosts).sort(sortfunction);
-    }
-
-    return hosts;
-  },
-
-  getHostItems: function (highlighted) {
-    return _.map(this.sortHosts(this.props.hosts), function (host) {
-      /* jshint trailing:false, quotmark:false, newcap:false */
-      /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
-      return (
-        <HostItem
-            key={host.id}
-            model={host}
-            columnHighlighted={highlighted} />
-      );
-      /* jshint trailing:true, quotmark:true, newcap:true */
-      /* jscs:enable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
-    });
+    // rely on default sorting
+    return null;
   },
 
   render: function () {
-    var sortKey = this.state.sortKey;
-    var highlighted = [];
-
-    var headerClassSet = React.addons.classSet({
-      "dropup": this.state.sortOrder === -1
-    });
-
-    var carets = _.reduce(["hostname", "tasks_size", "cpus", "mem", "disk"],
-        function(caret, key, i) {
-      if (key === sortKey) {
-        caret[key] = <span className="caret"></span>;
-        highlighted[i] = " highlighted";
-      } else {
-        highlighted[i] = "";
-      }
-      return caret;
-    }, {});
 
     /* jshint trailing:false, quotmark:false, newcap:false */
     /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th className={"clickable" + highlighted[0]}
-                onClick={this.sortBy.bind(null, "hostname")}>
-              <span className={headerClassSet}>
-                HOST NAME{carets.hostname}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[1]}
-                onClick={this.sortBy.bind(null, "tasks_size")}>
-              <span className={headerClassSet}>
-                TASKS{carets.tasks_size}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[2]}
-                onClick={this.sortBy.bind(null, "cpus")}>
-              <span className={headerClassSet}>
-                CPU{carets.cpus}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[3]}
-              onClick={this.sortBy.bind(null, "mem")}>
-              <span className={headerClassSet}>
-                MEM{carets.mem}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[4]}
-                onClick={this.sortBy.bind(null, "disk")}>
-              <span className={headerClassSet}>
-                DISK{carets.disk}
-              </span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.getHostItems(highlighted)}
-        </tbody>
-      </table>
+      <Table
+        className="table"
+        columns={columns}
+        keys={["hostname"]}
+        sortBy={{ prop: "hostname", order: "desc" }}
+        sortFunc={this.sortFunction}
+        dataArray={this.props.hosts} />
     );
   }
 });
--- src/js/components/ServiceList.jsx
+++ src/js/components/ServiceList.jsx
@@ -1,9 +1,114 @@
 /** @jsx React.DOM */
 
 var _ = require("underscore");
+var Humanize = require("humanize");
 var React = require("react/addons");
 
-var ServiceItem = require("./ServiceItem");
+var Table = require("./Table");
+
+function renderHeadline(prop) {
+  return (
+    <h5 className="flush-top flush-bottom">
+      <i className="icon icon-small icon-small-white border-radius"></i>
+      {prop}
+    </h5>
+  );
+}
+
+function renderHealth(prop, model) {
+  var status = "Active";
+  if (model.active !== true) {
+    status = "Inactive";
+  }
+
+  var statusClassSet = React.addons.classSet({
+    "collection-item-content-status": true,
+    "text-success": model.active,
+    "text-danger": !model.active
+  });
+
+  /* jshint trailing:false, quotmark:false, newcap:false */
+  /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
+  return (
+    <span className={statusClassSet}>{status}</span>
+  );
+  /* jshint trailing:true, quotmark:true, newcap:true */
+  /* jscs:enable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
+}
+
+function renderTask(prop, model) {
+  return (
+    <span>
+      {model[prop]}
+      <span className="mobile-displayed-text"> Tasks</span>
+    </span>
+  );
+}
+
+function renderStats(prop, model) {
+  var value = _.last(model.used_resources[prop]).value;
+  if(prop !== "cpus") {
+    value = Humanize.filesize(value * 1024 * 1024, 1024, 1);
+  }
+
+  return (
+    <span>
+      {value}
+    </span>
+  );
+}
+
+function classNameStats(prop, row, sortBy) {
+  var className = "align-right mobile-hidden";
+  if (sortBy.prop === prop) {
+    className += " highlighted";
+  }
+
+  return className;
+}
+
+var columns = [
+  {
+    prop: "name",
+    render: renderHeadline,
+    sortable: true,
+    title: "SERIVCE NAME"
+  },
+  {
+    prop: "active",
+    render: renderHealth,
+    sortable: true,
+    title: "HEALTH"
+  },
+  {
+    className: "align-right",
+    prop: "tasks_size",
+    render: renderTask,
+    sortable: true,
+    title: "TASKS"
+  },
+  {
+    className: classNameStats,
+    prop: "cpus",
+    render: renderStats,
+    sortable: true,
+    title: "CPU"
+  },
+  {
+    className: classNameStats,
+    prop: "mem",
+    render: renderStats,
+    sortable: true,
+    title: "MEM"
+  },
+  {
+    className: classNameStats,
+    prop: "disk",
+    render: renderStats,
+    sortable: true,
+    title: "DISK"
+  },
+];
 
 var ServicesList = React.createClass({
 
@@ -19,154 +124,32 @@ var ServicesList = React.createClass({
     };
   },
 
-  getInitialState: function() {
-    return {
-      sortKey: null,
-      sortOrder: null
-    };
-  },
+  sortFunction: function (prop) {
+    if (prop === "cpus" || prop === "mem" || prop === "disk") {
 
-  sortBy: function (comparator) {
-    var state = this.state;
-    var sortOrder;
-
-    if (state.sortKey !== comparator) {
-      sortOrder = 1;
-    }
-
-    if (state.sortKey == null || state.sortKey === comparator) {
-      if (state.sortOrder == null) {
-        sortOrder = 1;
-      } else if (state.sortOrder === 1) {
-        sortOrder = -1;
-      } else if (state.sortOrder === -1) {
-        sortOrder = null;
-        comparator = null;
-      }
-    }
-
-    this.setState({
-      sortKey: comparator,
-      sortOrder: sortOrder
-    });
-  },
-
-  sortServices: function (frameworks) {
-    var key = this.state.sortKey;
-    var order = this.state.sortOrder;
-    var sortfunction = function (a, b) {
-      if (a[key] > b[key]) {
-        return 1 * order;
-      }
-      if (a[key] < b[key]) {
-        return -1 * order;
-      }
-      return 0;
-    };
-
-    if (key === "cpus" || key === "mem" || key === "disk") {
-      sortfunction = function (a, b) {
-        a = _.last(a.used_resources[key]).value;
-        b = _.last(b.used_resources[key]).value;
-        if (a > b) {
-          return 1 * order;
-        }
-        if (a < b) {
-          return -1 * order;
-        }
-        return 0;
+      return function (a, b) {
+        a = _.last(a.used_resources[prop]).value;
+        b = _.last(b.used_resources[prop]).value;
+        return a < b ? -1 : a > b ? 1 : 0;
       };
     }
 
-    if (sortfunction != null) {
-      return _.clone(frameworks).sort(sortfunction);
-    }
-
-    return frameworks;
-  },
-
-  getServiceItems: function (highlighted) {
-    return _.map(this.sortServices(this.props.frameworks), function (service) {
-      /* jshint trailing:false, quotmark:false, newcap:false */
-      /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
-      return (
-        <ServiceItem
-            key={service.id}
-            model={service}
-            columnHighlighted={highlighted} />
-      );
-      /* jshint trailing:true, quotmark:true, newcap:true */
-      /* jscs:enable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
-    });
+    // rely on default sorting
+    return null;
   },
 
   render: function () {
-    var sortKey = this.state.sortKey;
-    var highlighted = [];
-
-    var headerClassSet = React.addons.classSet({
-      "dropup": this.state.sortOrder === -1
-    });
-
-    var carets = _.reduce(["name", "active", "tasks_size", "cpus", "mem", "disk"],
-        function(caret, key, i) {
-      if (key === sortKey) {
-        caret[key] = <span className="caret"></span>;
-        highlighted[i] = " highlighted";
-      } else {
-        highlighted[i] = "";
-      }
-      return caret;
-    }, {});
 
     /* jshint trailing:false, quotmark:false, newcap:false */
     /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th className={"clickable" + highlighted[0]}
-                onClick={this.sortBy.bind(null, "name")}>
-              <span className={headerClassSet}>
-                SERVICE NAME{carets.name}
-              </span>
-            </th>
-            <th className={"clickable" + highlighted[1]}
-                onClick={this.sortBy.bind(null, "active")}>
-              <span className={headerClassSet}>
-                HEALTH{carets.active}
-              </span>
-            </th>
-            <th className={"align-right clickable" + highlighted[2]}
-                onClick={this.sortBy.bind(null, "tasks_size")}>
-              <span className={headerClassSet}>
-                TASKS{carets.tasks_size}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[3]}
-                onClick={this.sortBy.bind(null, "cpus")}>
-              <span className={headerClassSet}>
-                CPU{carets.cpus}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[4]}
-                onClick={this.sortBy.bind(null, "mem")}>
-              <span className={headerClassSet}>
-                MEM{carets.mem}
-              </span>
-            </th>
-            <th className={"align-right fixed-width clickable" + highlighted[5]}
-                onClick={this.sortBy.bind(null, "disk")}>
-              <span className={headerClassSet}>
-                DISK{carets.disk}
-              </span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.getServiceItems(highlighted)}
-        </tbody>
-      </table>
+      <Table
+        className="table"
+        columns={columns}
+        keys={["name"]}
+        sortBy={{ prop: "name", order: "desc" }}
+        sortFunc={this.sortFunction}
+        dataArray={this.props.frameworks} />
     );
   }
 });
