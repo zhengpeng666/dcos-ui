95f274d4b3b72db21ab91c3084a5cc55944af18d
Felix Gertz <felix@mesosphere.io>
2015-03-19 14:32:28 +0100

    CSS transition for progress-bar.

--- src/js/components/charts/ProgressBar.jsx
+++ src/js/components/charts/ProgressBar.jsx
@@ -1,6 +1,5 @@
 /** @jsx React.DOM */
 
-var d3 = require("d3");
 var React = require("react/addons");
 
 var ProgressBar = React.createClass({
@@ -10,7 +9,6 @@ var ProgressBar = React.createClass({
   propTypes: {
     colorIndex: React.PropTypes.number,
     max: React.PropTypes.number,
-    tweenDuration: React.PropTypes.number,
     value: React.PropTypes.number.isRequired
   },
 
@@ -18,35 +16,26 @@ var ProgressBar = React.createClass({
     return {
       colorIndex: 1,
       max: 100,
-      tweenDuration: 600,
       value: 0
     };
   },
 
   componentDidMount: function () {
-    this.animate(this.props);
+    setTimeout(this.applyWidth.bind(this, this.props), 100);
   },
 
   componentDidUpdate: function () {
-    this.animate(this.props);
+    this.applyWidth(this.props);
   },
 
-  animate: function (props) {
+  applyWidth: function (props) {
     var bar = this.refs.bar.getDOMNode();
     var newValue = (props.value / props.max * 100) + "%";
     var oldValue = bar.style.width;
 
-    if (oldValue === newValue) {
-      return;
+    if (oldValue !== newValue) {
+      bar.style.width = newValue;
     }
-
-    d3.select(bar).interrupt()
-      .transition()
-        .duration(this.props.tweenDuration)
-        .ease("linear")
-        .styleTween("width", function () {
-          return d3.interpolate(oldValue, newValue);
-        });
   },
 
   render: function () {
