e8880c83a532a90e0f8f6906d7c56b9211b90c1e
Rafael Corral <me@rafaelcorral.com>
2015-03-19 16:50:45 -0700

    Create graph for task failure rate. Fixes: DCOS-619

--- src/js/stores/MesosStateStore.js
+++ src/js/stores/MesosStateStore.js
@@ -131,20 +131,70 @@ function getStatesByFramework() {
 //     state: "TASK_RUNNING"
 //   }, ...]
 // }]
-function getTasksByStatus(frameworks) {
-  return _.chain(frameworks)
-    .pluck("tasks")
-    .flatten()
-    .groupBy(function (task) {
-      return task.state;
-    })
-    .map(function (tasks, value) {
-      return {
-        state: value,
-        tasks: tasks
-      };
-    })
-    .value();
+function getTasksByStatus(frameworks, taskTypes) {
+  if (!taskTypes || frameworks.length === 0) {
+    return [];
+  }
+
+  var types = {};
+  // Loop through all frameworks
+  frameworks.forEach(function (framework) {
+    // Loop through the requested taskTypes
+    taskTypes.forEach(function (taskType) {
+      if (framework[taskType] === void 0) {
+        return;
+      }
+
+      // Loop through tasks in for the task type
+      framework[taskType].forEach(function (task) {
+        var state = task.state;
+        if (types[state] === void 0) {
+          types[state] = {
+            state: state,
+            tasks: []
+          };
+        }
+
+        types[state].tasks.push(task);
+      });
+    });
+  });
+
+  return _.values(types);
+}
+
+function getAllFailureRates (list, taskTypes) {
+  var failures = [];
+  _.each(list, function (state) {
+    var statuses = getTasksByStatus(state.frameworks, taskTypes);
+    var data = {
+      date: state.date,
+      states: {}
+    };
+
+    statuses.forEach(function (status) {
+      data.states[status.state] = status.tasks.length;
+    });
+
+    // refs: https://github.com/apache/mesos/blob/master/include/mesos/mesos.proto
+    var successful = (data.states.TASK_STAGING || 0) +
+      (data.states.TASK_STARTING || 0) +
+      (data.states.TASK_RUNNING || 0) +
+      (data.states.TASK_FINISHED || 0)
+      ;
+    var failed = (data.states.TASK_FAILED || 0) +
+      (data.states.TASK_KILLED || 0) +
+      (data.states.TASK_LOST || 0) +
+      (data.states.TASK_ERROR || 0)
+      ;
+
+    failures.push({
+      date: data.date,
+      rate: (failed / (successful + failed) * 100)|0
+    });
+  });
+
+  return failures;
 }
 
 // [{
@@ -395,7 +445,11 @@ var MesosStateStore = _.extend({}, EventEmitter.prototype, {
   },
 
   getTasks: function () {
-    return getTasksByStatus(this.getLatest().frameworks);
+    return getTasksByStatus(this.getLatest().frameworks, ["tasks"]);
+  },
+
+  getTaskFailureRate: function () {
+    return getAllFailureRates(_mesosStates, ["tasks", "completed_tasks"]);
   },
 
   getTotalResources: function () {
