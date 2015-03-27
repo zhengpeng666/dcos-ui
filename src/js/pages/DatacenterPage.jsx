zhengpeng666/dcos-ui

commit 751cecad2d69b11f87800217e8ae7a6a8e6a00ef

Fixing merge conflict errors

src/js/pages/DatacenterPage.jsx
@@ -5,26 +5,24 @@ var React = require("react/addons");
 
 var AlertPanel = require("../components/AlertPanel");
 var EventTypes = require("../constants/EventTypes");
-var FilterHeadline = require("../components/FilterHeadline");
 var FilterInputText = require("../components/FilterInputText");
 var MesosStateStore = require("../stores/MesosStateStore");
 var ResourceBarChart = require("../components/charts/ResourceBarChart");
 var SidebarActions = require("../events/SidebarActions");
 var SidebarToggle = require("./SidebarToggle");
-var Panel = require("../components/Panel");
 var HostTable = require("../components/HostTable");
-
 
 var DEFAULT_FILTER_OPTIONS = {searchString: ""};
 
 function getMesosHosts(filterOptions) {
   filterOptions = filterOptions || _.clone(DEFAULT_FILTER_OPTIONS);
   var hosts = MesosStateStore.getHosts(filterOptions);
+  var allHosts = MesosStateStore.getLatest().slaves;
   return _.extend({
     hosts: hosts,
     statesProcessed: MesosStateStore.getStatesProcessed(),
     refreshRate: MesosStateStore.getRefreshRate(),
-    totalHosts: MesosStateStore.getLatest().slaves.length,
+    allHosts: allHosts,
     totalHostsResources: MesosStateStore.getTotalHostsResources(hosts),
     totalResources: MesosStateStore.getTotalResources()
   }, {filterOptions: filterOptions});
@@ -73,6 +71,35 @@ var DatacenterPage = React.createClass({
     this.setState(getMesosHosts(filterOptions));
   },
 
+  getHostsStats: function () {
+    var state = this.state;
+    var filteredLength = state.hosts.length;
+    var totalLength = state.allHosts.length;
+
+    var filteredClassSet = React.addons.classSet({
+      "hidden": filteredLength === totalLength
+    });
+
+    var unfilteredClassSet = React.addons.classSet({
+      "hidden": filteredLength !== totalLength
+    });
+
+    /* jshint trailing:false, quotmark:false, newcap:false */
+    /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
+    return (
+      <div>
+        <h4 className={filteredClassSet}>
+          Showing {filteredLength} of {totalLength} Hosts
+        </h4>
+        <h4 className={unfilteredClassSet}>
+          {totalLength} Hosts
+        </h4>
+      </div>
+    );
+    /* jshint trailing:true, quotmark:true, newcap:true */
+    /* jscs:enable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
+  },
+
   getHostsPageContent: function () {
     var state = this.state;
 
@@ -118,7 +145,7 @@ var DatacenterPage = React.createClass({
 
   render: function () {
     var state = this.state;
-    var isEmpty = state.statesProcessed && state.totalHosts === 0;
+    var isEmpty = state.statesProcessed && state.allHosts.length === 0;
 
     /* jshint trailing:false, quotmark:false, newcap:false */
     /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */

src/js/pages/ServicesPage.jsx
@@ -5,13 +5,11 @@ var React = require("react/addons");
 
 var AlertPanel = require("../components/AlertPanel");
 var EventTypes = require("../constants/EventTypes");
-var FilterHeadline = require("../components/FilterHeadline");
 var FilterHealth = require("../components/FilterHealth");
 var FilterInputText = require("../components/FilterInputText");
 var MesosStateStore = require("../stores/MesosStateStore");
 var SidebarActions = require("../events/SidebarActions");
 var SidebarToggle = require("./SidebarToggle");
-var Panel = require("../components/Panel");
 var ResourceBarChart = require("../components/charts/ResourceBarChart");
 var ServiceTable = require("../components/ServiceTable");
 
@@ -34,7 +32,7 @@ function getMesosServices(filterOptions) {
   return {
     frameworks: frameworks,
     statesProcessed: MesosStateStore.getStatesProcessed(),
-    countByHealth: MesosStateStore.getCountByHealth(allFrameworks),
+    countByHealth: getCountByHealth(allFrameworks),
     refreshRate: MesosStateStore.getRefreshRate(),
     totalFrameworks: allFrameworks.length,
     totalFrameworksResources:
@@ -101,6 +99,35 @@ var ServicesPage = React.createClass({
   resetFilter: function () {
     var filterOptions = _.clone(DEFAULT_FILTER_OPTIONS);
     this.setState(getMesosServices(filterOptions));
+  },
+
+  getServiceStats: function () {
+    var state = this.state;
+    var filteredLength = state.frameworks.length;
+    var totalLength = state.totalFrameworks;
+
+    var filteredClassSet = React.addons.classSet({
+      "hidden": filteredLength === totalLength
+    });
+
+    var unfilteredClassSet = React.addons.classSet({
+      "hidden": filteredLength !== totalLength
+    });
+
+    /* jshint trailing:false, quotmark:false, newcap:false */
+    /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
+    return (
+      <div>
+        <h4 className={filteredClassSet}>
+          Showing {filteredLength} of {totalLength} Services
+        </h4>
+        <h4 className={unfilteredClassSet}>
+          {totalLength} Services
+        </h4>
+      </div>
+    );
+    /* jshint trailing:true, quotmark:true, newcap:true */
+    /* jscs:enable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
   },
 
   getServicesPageContent: function () {

commit 303ae819db2e5b97fc8bf2e86324229b2317e438

Fixing filter options bug introduced with merge https://github.com/mesosphere/dcos-ui/commit/751cecad2d69b11f87800217e8ae7a6a8e6a00ef

src/js/pages/DatacenterPage.jsx
@@ -6,6 +6,7 @@ var React = require("react/addons");
 var AlertPanel = require("../components/AlertPanel");
 var EventTypes = require("../constants/EventTypes");
 var FilterInputText = require("../components/FilterInputText");
+var FilterHeadline = require("../components/FilterHeadline");
 var MesosStateStore = require("../stores/MesosStateStore");
 var ResourceBarChart = require("../components/charts/ResourceBarChart");
 var SidebarActions = require("../events/SidebarActions");
@@ -71,35 +72,6 @@ var DatacenterPage = React.createClass({
     this.setState(getMesosHosts(filterOptions));
   },
 
-  getHostsStats: function () {
-    var state = this.state;
-    var filteredLength = state.hosts.length;
-    var totalLength = state.allHosts.length;
-
-    var filteredClassSet = React.addons.classSet({
-      "hidden": filteredLength === totalLength
-    });
-
-    var unfilteredClassSet = React.addons.classSet({
-      "hidden": filteredLength !== totalLength
-    });
-
-    /* jshint trailing:false, quotmark:false, newcap:false */
-    /* jscs:disable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
-    return (
-      <div>
-        <h4 className={filteredClassSet}>
-          Showing {filteredLength} of {totalLength} Hosts
-        </h4>
-        <h4 className={unfilteredClassSet}>
-          {totalLength} Hosts
-        </h4>
-      </div>
-    );
-    /* jshint trailing:true, quotmark:true, newcap:true */
-    /* jscs:enable disallowTrailingWhitespace, validateQuoteMarks, maximumLineLength */
-  },
-
   getHostsPageContent: function () {
     var state = this.state;
 
@@ -112,9 +84,13 @@ var DatacenterPage = React.createClass({
           resources={state.totalHostsResources}
           totalResources={state.totalResources}
           refreshRate={state.refreshRate} />
-        {this.getHostsStats()}
+        <FilterHeadline
+          onReset={this.resetFilter}
+          name="Hosts"
+          currentLength={state.hosts.length}
+          totalLength={state.allHosts.length} />
         <FilterInputText
-          searchString={this.state.searchString}
+          searchString={this.state.filterOptions.searchString}
           onSubmit={this.onFilterChange} />
         <HostTable hosts={this.state.hosts} />
       </div>

src/js/pages/ServicesPage.jsx
@@ -6,6 +6,7 @@ var React = require("react/addons");
 var AlertPanel = require("../components/AlertPanel");
 var EventTypes = require("../constants/EventTypes");
 var FilterHealth = require("../components/FilterHealth");
+var FilterHeadline = require("../components/FilterHeadline");
 var FilterInputText = require("../components/FilterInputText");
 var MesosStateStore = require("../stores/MesosStateStore");
 var SidebarActions = require("../events/SidebarActions");
@@ -143,18 +144,22 @@ var ServicesPage = React.createClass({
           totalResources={state.totalResources}
           refreshRate={state.refreshRate}
           resourceType="Services" />
-        {this.getServiceStats()}
+        <FilterHeadline
+          onReset={this.resetFilter}
+          name="Services"
+          currentLength={state.frameworks.length}
+          totalLength={state.totalFrameworks} />
         <ul className="list list-unstyled list-inline flush-bottom">
           <li>
             <FilterHealth
               countByHealth={state.countByHealth}
-              healthFilter={state.healthFilter}
+              healthFilter={state.filterOptions.healthFilter}
               onSubmit={this.onChangeHealthFilter}
               servicesLength={state.totalFrameworks} />
           </li>
           <li>
             <FilterInputText
-              searchString={state.searchString}
+              searchString={state.filterOptions.searchString}
               onSubmit={this.onChange} />
           </li>
         </ul>