a32f5399e27c409e3443aaf7c6c3e0c280ea862f
mlunoe <michael.lunoe@gmail.com>
2015-04-06 17:03:02 -0700

    Handling upper case letters in true stings

--- src/js/stores/MesosStateStore.js
+++ src/js/stores/MesosStateStore.js
@@ -598,7 +598,7 @@ var MesosStateStore = _.extend({}, EventEmitter.prototype, {
 
   processMarathonHealth: function (data) {
     _frameworkHealth = _.foldl(data.apps, function (curr, app) {
-      if (app.labels.DCOS_PACKAGE_IS_FRAMEWORK !== "true" ||
+      if (app.labels.DCOS_PACKAGE_IS_FRAMEWORK.toLowerCase() !== "true" ||
           app.healthChecks == null ||
           app.healthChecks.length === 0) {
         return curr;
