e933b49ac694d193ad9628ea268065ded19f1a10
Felix Gertz <felix@mesosphere.io>
2015-04-16 15:46:02 +0200

    Check for el.dataset to not break IE10

--- src/js/mixins/TooltipMixin.js
+++ src/js/mixins/TooltipMixin.js
@@ -2,34 +2,6 @@ var _ = require("underscore");
 
 var Tooltip = require("../vendor/tooltip");
 
-function getDataAttribute(el, key) {
-  if (el.dataset === undefined) {
-    return el.getAttribute("data-" + key);
-  }
-
-  return el.dataset[key];
-}
-
-function setDataAttribute(el, key, value) {
-  if (el.dataset === undefined) {
-    el.setAttribute("data-" + key, value);
-  } else {
-    el.dataset[key] = value;
-  }
-}
-
-function removeDataAttribute(el, key) {
-  if (el == null) {
-    return;
-  }
-
-  if (el.dataset === undefined) {
-    el.removeAttribute("data-" + key);
-  } else {
-    delete el.dataset[key];
-  }
-}
-
 var TooltipMixin = {
   componentDidMount: function () {
     this.tips = {};
@@ -51,9 +23,9 @@ var TooltipMixin = {
   tip_handleContainerMouseMove: function (e) {
     var el = e.target;
 
-    if (getDataAttribute(el, "behavior") && getDataAttribute(el, "behavior") === "show-tip") {
-      var tip = this.tips[getDataAttribute(el, "tipID")];
-      tip.content(getDataAttribute(el, "tipContent"));
+    if (el.dataset && el.dataset.behavior && el.dataset.behavior === "show-tip") {
+      var tip = this.tips[el.dataset.tipID];
+      tip.content(el.dataset.tipContent);
       tip.show();
       el.addEventListener("mouseleave", this.tip_handleMouseLeave);
     }
@@ -62,8 +34,8 @@ var TooltipMixin = {
   tip_handleMouseLeave: function (e) {
     var el = e.target;
 
-    if (this.tips[getDataAttribute(el, "tipID")]) {
-      this.tips[getDataAttribute(el, "tipID")].hide();
+    if (el.dataset && this.tips[el.dataset.tipID]) {
+      this.tips[el.dataset.tipID].hide();
     }
 
     el.removeEventListener("mouseleave", this.tip_handleMouseLeave);
@@ -78,10 +50,12 @@ var TooltipMixin = {
 
     for (var i = selected.length - 1; i >= 0; i--) {
       el = selected[i];
-      if (getDataAttribute(el, "tipID")) {
-        found.push(getDataAttribute(el, "tipID"));
-      } else if (getDataAttribute(el, "tipContent")) {
-        newTips.push(this.tip_createTipForElement(el));
+      if (el.dataset) {
+        if (el.dataset.tipID) {
+          found.push(el.dataset.tipID);
+        } else if (el.dataset.tipContent) {
+          newTips.push(this.tip_createTipForElement(el));
+        }
       }
     }
 
@@ -96,26 +70,29 @@ var TooltipMixin = {
   },
 
   tip_createTipForElement: function (el) {
-    setDataAttribute(el, "tipID", _.uniqueId("tooltip"));
-    var options = {};
-    if (getDataAttribute(el, "tipPlace")) {
-      options.place = getDataAttribute(el, "tipPlace");
+    if (el.dataset) {
+      el.dataset.tipID = _.uniqueId("tooltip");
+      var options = {};
+      if (el.dataset.tipPlace) {
+        options.place = el.dataset.tipPlace;
+      }
+
+      var tip = new Tooltip(undefined, options);
+      tip.attach(el);
+
+      this.tips[el.dataset.tipID] = tip;
+
+      return el.dataset.tipID;
     }
-
-    var tip = new Tooltip(undefined, options);
-    tip.attach(el);
-
-    this.tips[getDataAttribute(el, "tipID")] = tip;
-
-    return getDataAttribute(el, "tipID");
   },
 
   tip_destroyTip: function (tipID) {
     // Allows us to create a new tip for the element.
     // Useful when the element has tooltip -> doesn't -> then has it again.
     var el = this.getDOMNode().querySelector("[data-tip-i-d=" + tipID + "]");
-
-    removeDataAttribute(el, "tipID");
+    if (el && el.dataset) {
+      delete el.dataset.tipID;
+    }
 
     this.tips[tipID].destroy();
     delete this.tips[tipID];
