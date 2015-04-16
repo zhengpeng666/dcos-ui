c0c837cf9f29cb656f57b3a6531073308cf0397b
Felix Gertz <felix@mesosphere.io>
2015-04-16 11:43:20 +0200

    Check for TeamCity env

--- jest/setupTestFramework.js
+++ jest/setupTestFramework.js
@@ -1,6 +1,4 @@
-jasmine.VERBOSE = true;
-
-var jasmineEnv = jasmine.getEnv();
-var TeamCityReporter = require("../node_modules/jasmine-reporters/src/jasmine.teamcity_reporter");
-
-jasmineEnv.addReporter(new jasmine.TeamcityReporter());
+if (process.env.TEAMCITY_VERSION != null)  {
+  require("../node_modules/jasmine-reporters/src/jasmine.teamcity_reporter");
+  jasmine.getEnv().addReporter(new jasmine.TeamcityReporter());
+}
