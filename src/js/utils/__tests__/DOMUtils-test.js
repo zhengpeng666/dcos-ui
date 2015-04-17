8e455d1f10a1a51a0c41f6cdda31d0b631714548
Rafael Corral <me@rafaelcorral.com>
2015-04-17 15:25:32 -0700

    namespacing tests, gotta practice what I preach ;)

--- src/js/utils/__tests__/DOMUtils-test.js
+++ src/js/utils/__tests__/DOMUtils-test.js
@@ -4,80 +4,83 @@ var DOMUtils = require("../DOMUtils");
 
 describe("DOMUtils", function () {
 
-  function buildElement (style) {
-    return "<div style=\"" + style.join(";") + "\"></div>";
-  }
+  describe("#getComputedWidth", function () {
+    function buildElement (style) {
+      return "<div style=\"" + style.join(";") + "\"></div>";
+    }
 
-  beforeEach(function () {
-    this.style = [
-      "width: 100px"
-    ];
-  });
+    beforeEach(function () {
+      this.style = [
+        "width: 100px"
+      ];
+    });
 
-  it("calculates left padding", function () {
-    this.style.push("padding-left: 1px");
-    document.body.innerHTML = buildElement(this.style);
+    it("calculates left padding", function () {
+      this.style.push("padding-left: 1px");
+      document.body.innerHTML = buildElement(this.style);
 
-    // jsdom doesn't calculate
-    document.querySelector("div").offsetWidth = 101;
+      // jsdom doesn't calculate
+      document.querySelector("div").offsetWidth = 101;
 
-    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
-    expect(width).toEqual(100);
-  });
+      var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+      expect(width).toEqual(100);
+    });
 
-  it("calculates right padding", function () {
-    this.style.push("padding-right: 1px");
-    document.body.innerHTML = buildElement(this.style);
-    document.querySelector("div").offsetWidth = 101;
+    it("calculates right padding", function () {
+      this.style.push("padding-right: 1px");
+      document.body.innerHTML = buildElement(this.style);
+      document.querySelector("div").offsetWidth = 101;
 
-    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
-    expect(width).toEqual(100);
-  });
+      var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+      expect(width).toEqual(100);
+    });
 
-  it("calculates left border", function () {
-    this.style.push("border-left-width: 1px");
-    document.body.innerHTML = buildElement(this.style);
-    document.querySelector("div").offsetWidth = 101;
+    it("calculates left border", function () {
+      this.style.push("border-left-width: 1px");
+      document.body.innerHTML = buildElement(this.style);
+      document.querySelector("div").offsetWidth = 101;
 
-    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
-    expect(width).toEqual(100);
-  });
+      var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+      expect(width).toEqual(100);
+    });
 
-  it("calculates right border", function () {
-    this.style.push("border-right-width: 1px");
-    document.body.innerHTML = buildElement(this.style);
-    document.querySelector("div").offsetWidth = 101;
+    it("calculates right border", function () {
+      this.style.push("border-right-width: 1px");
+      document.body.innerHTML = buildElement(this.style);
+      document.querySelector("div").offsetWidth = 101;
 
-    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
-    expect(width).toEqual(100);
-  });
+      var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+      expect(width).toEqual(100);
+    });
 
-  it("calculates computed width", function () {
-    this.style.push(
-      "padding-left: 1px",
-      "padding-right: 1px",
-      "border-left-width: 1px",
-      "border-right-width: 1px"
-    );
-    document.body.innerHTML = buildElement(this.style);
-    document.querySelector("div").offsetWidth = 104;
+    it("calculates computed width", function () {
+      this.style.push(
+        "padding-left: 1px",
+        "padding-right: 1px",
+        "border-left-width: 1px",
+        "border-right-width: 1px"
+      );
+      document.body.innerHTML = buildElement(this.style);
+      document.querySelector("div").offsetWidth = 104;
 
-    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
-    expect(width).toEqual(100);
-  });
+      var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+      expect(width).toEqual(100);
+    });
 
-  it("does not calculate unnecessary properties", function () {
-    this.style.push(
-      "padding-top: 1px",
-      "padding-bottom: 1px",
-      "border-top-width: 1px",
-      "border-bottom-width: 1px"
-    );
-    document.body.innerHTML = buildElement(this.style);
-    document.querySelector("div").offsetWidth = 100;
+    it("does not calculate unnecessary properties", function () {
+      this.style.push(
+        "padding-top: 1px",
+        "padding-bottom: 1px",
+        "border-top-width: 1px",
+        "border-bottom-width: 1px"
+      );
+      document.body.innerHTML = buildElement(this.style);
+      document.querySelector("div").offsetWidth = 100;
 
-    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
-    expect(width).toEqual(100);
+      var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+      expect(width).toEqual(100);
+    });
+
   });
 
 });
