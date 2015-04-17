7a115ff1a0cb21e04a7ec7728557fdf60699308f
Rafael Corral <me@rafaelcorral.com>
2015-04-17 14:30:12 -0700

    Adding tests for utils/DOMUtils

---
+++ src/js/utils/__tests__/DOMUtils-test.js
@@ -0,0 +1,69 @@
+jest.dontMock("../DOMUtils");
+
+var DOMUtils = require("../DOMUtils");
+
+describe("DOMUtils", function () {
+
+  function buildElement (style) {
+    return "<div style=\"" + style.join(";") + "\"></div>";
+  }
+
+  beforeEach(function () {
+    this.style = [
+      "width: 100px"
+    ];
+  });
+
+  it("calculates left padding", function () {
+    this.style.push("padding-left: 1px");
+    document.body.innerHTML = buildElement(this.style);
+
+    // jsdom doesn't calculate
+    document.querySelector("div").offsetWidth = 101;
+
+    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+    expect(width).toEqual(100);
+  });
+
+  it("calculates right padding", function () {
+    this.style.push("padding-right: 1px");
+    document.body.innerHTML = buildElement(this.style);
+    document.querySelector("div").offsetWidth = 101;
+
+    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+    expect(width).toEqual(100);
+  });
+
+  it("calculates left border", function () {
+    this.style.push("border-left-width: 1px");
+    document.body.innerHTML = buildElement(this.style);
+    document.querySelector("div").offsetWidth = 101;
+
+    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+    expect(width).toEqual(100);
+  });
+
+  it("calculates right border", function () {
+    this.style.push("border-right-width: 1px");
+    document.body.innerHTML = buildElement(this.style);
+    document.querySelector("div").offsetWidth = 101;
+
+    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+    expect(width).toEqual(100);
+  });
+
+  it("calculates computed width", function () {
+    this.style.push(
+      "padding-left: 1px",
+      "padding-right: 1px",
+      "border-left-width: 1px",
+      "border-right-width: 1px"
+    );
+    document.body.innerHTML = buildElement(this.style);
+    document.querySelector("div").offsetWidth = 104;
+
+    var width = DOMUtils.getComputedWidth(document.querySelector("div"));
+    expect(width).toEqual(100);
+  });
+
+});
