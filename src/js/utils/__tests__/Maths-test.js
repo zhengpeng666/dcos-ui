07f68391d28d875241da88777e3a0e61cdd631e2
Rafael Corral <me@rafaelcorral.com>
2015-04-17 02:59:23 -0700

    Added tests for utils/Maths. refs: DCOS-695

--- src/js/utils/__tests__/Maths-test.js
+++ src/js/utils/__tests__/Maths-test.js
@@ -1,7 +1,8 @@
 jest.dontMock("../Maths");
 
+var Maths = require("../Maths");
+
 describe("Maths", function () {
-  var Maths = require("../Maths");
 
   describe("#round", function () {
 
@@ -18,6 +19,59 @@ describe("Maths", function () {
     it("should return 0 with a non number", function () {
       var value = Maths.round(null);
       expect(value).toBe(0);
+    });
+
+  });
+
+  describe("#mapValue", function () {
+
+    it("should return min if there is no range", function () {
+      var value = Maths.mapValue(1, {min: 1, max: 1});
+      expect(value).toEqual(1);
+    });
+
+    it("should return undefined with a non number", function () {
+      var value = Maths.mapValue(NaN, {min: 0, max: 10});
+      expect(typeof value).toEqual("undefined");
+    });
+
+    it("should return a number", function () {
+      var value = Maths.mapValue(5, {min: 0, max: 10});
+      expect(typeof value).toEqual("number");
+    });
+
+    it("should map correctly", function () {
+      var value = Maths.mapValue(5, {min: 0, max: 10});
+      expect(value).toEqual(0.5);
+    });
+
+    it("should map correctly", function () {
+      var value = Maths.mapValue(2.5, {min: 0, max: 10});
+      expect(value).toEqual(0.25);
+    });
+
+  });
+
+  describe("#unmapValue", function () {
+
+    it("should return undefined with a non number", function () {
+      var value = Maths.unmapValue(NaN, {min: 0, max: 10});
+      expect(typeof value).toEqual("undefined");
+    });
+
+    it("should return a number", function () {
+      var value = Maths.unmapValue(0.5, {min: 0, max: 10});
+      expect(typeof value).toEqual("number");
+    });
+
+    it("should map correctly", function () {
+      var value = Maths.unmapValue(0.5, {min: 0, max: 10});
+      expect(value).toEqual(5);
+    });
+
+    it("should map correctly", function () {
+      var value = Maths.unmapValue(0.25, {min: 0, max: 10});
+      expect(value).toEqual(2.5);
     });
 
   });
