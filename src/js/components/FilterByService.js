74fa2e1693b4c434da4d46bd7fbf8709b4f41b0a
mlunoe <michael.lunoe@gmail.com>
2015-04-17 12:25:11 -0700

    Using objects with render functions

--- src/js/components/FilterByService.js
+++ src/js/components/FilterByService.js
@@ -5,7 +5,7 @@ var React = require("react/addons");
 
 var Dropdown = require("./Dropdown");
 
-var defaultKey = "default";
+var defaultId = "default";
 
 var FilterByService = React.createClass({
 
@@ -20,68 +20,77 @@ var FilterByService = React.createClass({
 
   getDefaultProps: function () {
     return {
-      byServiceFilter: defaultKey,
+      byServiceFilter: defaultId,
       services: [],
       totalHostsCount: 0,
       handleFilterChange: _.noop
     };
   },
 
-  handleItemSelection: function (key) {
-    if (key === defaultKey) {
+  handleItemSelection: function (obj) {
+    if (obj.id === defaultId) {
       this.props.handleFilterChange(null);
     } else {
-      this.props.handleFilterChange(key);
+      this.props.handleFilterChange(obj.id);
     }
   },
 
-  getSelectedItem: function (key, children) {
-    if (key === defaultKey) {
-      return (
-        <span className="badge-container">
-          <span>Filter by Service</span>
-        </span>
-      );
-    }
+  selectedRender: function () {
+    return (
+      <span className="badge-container">
+        <span>Filter by Service</span>
+      </span>
+    );
+  },
 
-    return _.find(children, function (item) {
-      return item.key === key;
-    });
+  itemRender: function(obj) {
+    return (
+      <span className="badge-container">
+        <span>{obj.name}</span>
+        <span className="badge">{obj.slaves_count}</span>
+      </span>
+    );
   },
 
   getDropdownItems: function () {
     var items = [
       {
-        id: defaultKey,
+        id: defaultId,
         name: "All Services",
         slaves_count: this.props.totalHostsCount
       }
     ].concat(this.props.services);
 
+    var itemRender = this.itemRender;
     return _.map(items, function (service) {
-      return (
-        <span key={service.id} className="badge-container">
-          <span>{service.name}</span>
-          <span className="badge">{service.slaves_count}</span>
-        </span>
-      );
-    });
+      var selectedRender;
+      if (service.id === defaultId) {
+        selectedRender = this.selectedRender;
+      }
+
+      return {
+        id: service.id,
+        name: service.name,
+        render: itemRender,
+        selectedRender: selectedRender,
+        slaves_count: service.slaves_count
+      };
+    }, this);
   },
 
-  getSelectedKey: function (key) {
-    if (key == null) {
-      return defaultKey;
+  getSelectedId: function (id) {
+    if (id == null) {
+      return defaultId;
     }
 
-    return key;
+    return id;
   },
 
   render: function () {
     return (
       <Dropdown
-        selectedKey={this.getSelectedKey(this.props.byServiceFilter)}
+        selectedId={this.getSelectedId(this.props.byServiceFilter)}
         handleItemSelection={this.handleItemSelection}
-        getSelectedItem={this.getSelectedItem}
         items={this.getDropdownItems()} />
     );
   }
