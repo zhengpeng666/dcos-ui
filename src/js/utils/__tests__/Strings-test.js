84241dca5c96d953b0959e68b3a40f7bcc57aefa
Rafael Corral <me@rafaelcorral.com>
2015-04-20 10:54:51 -0700

    Updated name of method

--- src/js/utils/__tests__/Strings-test.js
+++ src/js/utils/__tests__/Strings-test.js
@@ -4,15 +4,15 @@ var Strings = require("../Strings");
 
 describe("Strings", function () {
 
-  describe("#ipToHostName", function () {
+  describe("#ipToHostAddress", function () {
 
     it("converts ip string to hostname", function () {
-      var hostname = Strings.ipToHostName("ip-12-12-12-12");
+      var hostname = Strings.ipToHostAddress("ip-12-12-12-12");
       expect(hostname).toEqual("12.12.12.12");
     });
 
     it("does not convert ip string to hostname", function () {
-      var hostname = Strings.ipToHostName("12-12-12-12");
+      var hostname = Strings.ipToHostAddress("12-12-12-12");
       expect(hostname).toEqual("12-12-12-12");
     });
 
