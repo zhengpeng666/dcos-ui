9ad8ae474e140195e69694d78a200b55cba34ea5
Rafael Corral <me@rafaelcorral.com>
2015-05-07 22:29:28 -0700

    Removing trailing comma.

--- src/js/config/Config.template.js
+++ src/js/config/Config.template.js
@@ -3,7 +3,7 @@ var ConfigDev = {
   analyticsKey: "39uhSEOoRHMw6cMR6st9tYXDbAL3JSaP",
   disableLoginModal: false,
   rootUrl: "http://localhost:5080",
-  historyServer: "http://localhost:5080",
+  historyServer: "http://localhost:5080"
 };
 
 module.exports = ConfigDev;
