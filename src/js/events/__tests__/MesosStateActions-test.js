601673aa32a92dc50adc1d9abe696e045d7abed7
Rafael Corral <me@rafaelcorral.com>
2015-05-07 21:36:50 -0700

    Fixing tests.

--- src/js/events/__tests__/MesosStateActions-test.js
+++ src/js/events/__tests__/MesosStateActions-test.js
@@ -19,25 +19,25 @@ describe("Mesos State Actions", function () {
     it("fetches the most recent state by default", function () {
       MesosStateActions.fetch();
       expect($.ajax).toHaveBeenCalled();
-      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/history/last");
+      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/dcos-history-service/history/last");
     });
 
     it("fetches a whole minute when instructed", function () {
       MesosStateActions.fetch(TimeScales.MINUTE);
       expect($.ajax).toHaveBeenCalled();
-      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/history/minute");
+      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/dcos-history-service/history/minute");
     });
 
     it("fetches a whole hour when instructed", function () {
       MesosStateActions.fetch(TimeScales.HOUR);
       expect($.ajax).toHaveBeenCalled();
-      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/history/hour");
+      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/dcos-history-service/history/hour");
     });
 
     it("fetches a whole day when instructed", function () {
       MesosStateActions.fetch(TimeScales.DAY);
       expect($.ajax).toHaveBeenCalled();
-      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/history/day");
+      expect($.ajax.mostRecentCall.args[0].url).toEqual("http://historyserver/dcos-history-service/history/day");
     });
   });
 
