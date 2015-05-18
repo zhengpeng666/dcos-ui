dc992de3ed30b49936ee3cb2b0271b2ad529a360
Felix Gertz <felix@mesosphere.io>
2015-05-18 14:41:02 +0200

    Removing the resource bar chart legend

--- src/js/components/charts/ResourceBarChart.js
+++ src/js/components/charts/ResourceBarChart.js
@@ -92,22 +92,6 @@ var ResourceBarChart = React.createClass({
     );
   },
 
-  getLegend: function (info) {
-    var classSet = React.addons.classSet({
-      "button button-small button-stroke button-inverse": true,
-      "disabled": true
-    });
-
-    return (
-      <div className="button-group legend">
-        <button className={classSet}>
-          <span className={"line path-color-" + info.colorIndex}></span>
-          <span>{info.label} Allocated</span>
-        </button>
-      </div>
-    );
-  },
-
   getHeadline: function (info) {
     var headline = info.label + " Allocation Rate";
 
@@ -134,9 +118,6 @@ var ResourceBarChart = React.createClass({
           </div>
           <div className="panel-title">
             {this.getHeadline(info)}
-          </div>
-          <div className="panel-options-right">
-            {this.getLegend(info)}
           </div>
         </div>
         <div className="panel-content" ref="panelContent">
