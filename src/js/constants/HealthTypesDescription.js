f9ddb023c3689dade010dbc4bddee56a66d137a1
Felix Gertz <felix@mesosphere.io>
2015-06-02 10:34:27 +0200

    Better wording for 'unhealthy' mousover text

--- src/js/constants/HealthTypesDescription.js
+++ src/js/constants/HealthTypesDescription.js
@@ -1,6 +1,5 @@
 var HealthTypesDescription = {
-  UNHEALTHY: "There are some issues with this service. " +
-    "You should look into them immediately.",
+  UNHEALTHY: "One or more health checks are failing.",
   HEALTHY: "This service is operating normally.",
   IDLE: "This service has no running tasks.",
   NA: "No health checks are set up for this service."
