3b13c04e3686ec19799faa4c52bab047f49d0107
Kenny Tran <kenny8tran@gmail.com>
2015-07-27 12:59:42 -0700

    Do not save height in state

--- src/js/utils/DOMUtils.js
+++ src/js/utils/DOMUtils.js
@@ -40,6 +40,16 @@ var DOMUtils = {
     };
   },
 
+  getPageHeight: function () {
+    var body = document.body;
+    var html = document.documentElement;
+
+    var height = Math.max(body.scrollHeight, body.offsetHeight,
+                          html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+    return height;
+  },
+
   whichTransitionEvent: function (el) {
     var transitions = {
       "transition": "transitionend",
