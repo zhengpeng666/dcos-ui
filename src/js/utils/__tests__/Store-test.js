dc1946874b3cbaf1f2f04aefb21e09291845a01e
mlunoe <michael.lunoe@gmail.com>
2015-08-18 17:17:12 -0700

    Making tests a bit better

--- src/js/utils/__tests__/Store-test.js
+++ src/js/utils/__tests__/Store-test.js
@@ -1,26 +1,19 @@
 var _ = require("underscore");
 var EventEmitter = require("events").EventEmitter;
 
-jest.dontMock("../../mixins/GetSetMixin");
 jest.dontMock("../../utils/Store");
 
-var GetSetMixin = require("../../mixins/GetSetMixin");
 var Store = require("../../utils/Store");
-
-var EventEmitterProps = [
-  "addListener",
-  "emit",
-  "listeners",
-  "on",
-  "once",
-  "removeAllListeners",
-  "removeListener",
-  "setMaxListeners"
-];
 
 describe("Store", function () {
 
   describe("#createStore", function () {
+
+    beforeEach(function () {
+      this.emitterProps = _.sortBy(Object.keys(EventEmitter.prototype), function (el) {
+        return el;
+      });
+    });
 
     it("should return a store if given store is undefined", function () {
       var newStore = Store.createStore();
@@ -28,37 +21,26 @@ describe("Store", function () {
       expect(typeof newStore).toEqual("object");
     });
 
-    it("should not modify the EventEmitter", function () {
-      var newStore = Store.createStore({
-        someProperty: "someValue"
-      });
-
-      expect(EventEmitter).not.toEqual(newStore);
-    });
-
     it("should be able to create a store without mixins", function () {
+      var emitterProps = this.emitterProps;
       var newStore = Store.createStore({});
       var props = _.sortBy(Object.keys(newStore), function (el) {
         return el;
       });
       props.map(function (key, index) {
-        expect(key).toEqual(EventEmitterProps[index]);
+        expect(key).toEqual(emitterProps[index]);
       });
     });
 
     it("should be able to create a store with 1 mixin", function () {
+      var emitterProps = this.emitterProps;
       var newStore = Store.createStore({
-        mixins: [GetSetMixin]
+        mixins: [{someProperty: "someValue"}]
       });
 
       Object.keys(newStore).map(function (key, index) {
-        if (EventEmitterProps[index] == null) {
-          expect(
-            key === "get" ||
-            key === "getSet_data" ||
-            key === "set" ||
-            key === "mixins"
-          ).toEqual(true);
+        if (emitterProps[index] == null) {
+          expect(key === "someProperty" || key === "mixins").toEqual(true);
         }
       });
     });
