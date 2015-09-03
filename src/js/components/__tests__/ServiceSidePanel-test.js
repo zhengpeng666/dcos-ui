12fd0357814d75d2cc9ed8140c170df83554900c
mlunoe <michael.lunoe@gmail.com>
2015-09-03 15:54:44 -0700

    Removing unnecessary dontMocks

--- src/js/components/__tests__/ServiceSidePanel-test.js
+++ src/js/components/__tests__/ServiceSidePanel-test.js
@@ -1,18 +1,5 @@
-jest.dontMock("../../events/AppDispatcher");
-jest.dontMock("../../constants/ActionTypes");
-jest.dontMock("../../config/Config");
-jest.dontMock("../../constants/EventTypes");
-jest.dontMock("../../mixins/GetSetMixin");
-jest.dontMock("../../mixins/InternalStorageMixin");
-jest.dontMock("../../stores/MesosSummaryStore");
 jest.dontMock("../ServiceSidePanel");
-jest.dontMock("../../structs/SummaryList");
-jest.dontMock("../../structs/StateSummary");
-jest.dontMock("../../utils/MesosSummaryUtil");
-jest.dontMock("../../utils/RequestUtil");
-jest.dontMock("../../utils/StringUtil");
 jest.dontMock("../../utils/Store");
-jest.dontMock("../../utils/StringUtil");
 
 var React = require("react/addons");
 var TestUtils = React.addons.TestUtils;
