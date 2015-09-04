d24a7c830b73cfe72208993b9861c5f12f8bb391
Kenny Tran <kenny8tran@gmail.com>
2015-09-04 10:17:24 -0700

    Format code in HealthStatus better

--- src/js/constants/HealthStatus.js
+++ src/js/constants/HealthStatus.js
@@ -1,10 +1,26 @@
 import HealthTypes from "./HealthTypes";
 
 const HealthStatus = {
-  NA: {key: "NA", value: HealthTypes.NA, classes: {"text-mute": true}},
-  HEALTHY: {key: "HEALTHY", value: HealthTypes.HEALTHY, classes: {"text-success": true}},
-  UNHEALTHY: {key: "UNHEALTHY", value: HealthTypes.UNHEALTHY, classes: {"text-danger": true}},
-  IDLE: {key: "IDLE", value: HealthTypes.IDLE, classes: {"text-warning": true}}
+  NA: {
+    key: "NA",
+    value: HealthTypes.NA,
+    classes: {"text-mute": true}
+  },
+  HEALTHY: {
+    key: "HEALTHY",
+    value: HealthTypes.HEALTHY,
+    classes: {"text-success": true}
+  },
+  UNHEALTHY: {
+    key: "UNHEALTHY",
+    value: HealthTypes.UNHEALTHY,
+    classes: {"text-danger": true}
+  },
+  IDLE: {
+    key: "IDLE",
+    value: HealthTypes.IDLE,
+    classes: {"text-warning": true}
+  }
 };
 
 module.exports = HealthStatus;
