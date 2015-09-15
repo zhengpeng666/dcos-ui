d6f8d3659544b159bd4150dc14a2f711ae88af34
Kenny Tran <kenny8tran@gmail.com>
2015-09-15 09:24:36 -0700

    First pass at TaskSidePanel working

--- src/js/components/TaskTable.js
+++ src/js/components/TaskTable.js
@@ -210,6 +210,10 @@ export default class TaskTable extends React.Component {
   }
 }
 
+TaskTable.contextTypes = {
+  router: React.PropTypes.func
+};
+
 TaskTable.propTypes = {
   tasks: React.PropTypes.array.isRequired
 };
