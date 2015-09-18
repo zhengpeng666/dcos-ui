f454c042abb8e3ca92e3cbecbe18802aab8809dd
Kenny Tran <kenny8tran@gmail.com>
2015-09-18 13:44:04 -0700

    Fix tests

--- src/js/components/__tests__/TaskTable-test.js
+++ src/js/components/__tests__/TaskTable-test.js
@@ -1,7 +1,7 @@
 jest.dontMock("./fixtures/MockTasks");
+jest.dontMock("../../utils/ResourceTableUtil");
 jest.dontMock("../TaskTable");
 
-var _ = require("underscore");
 var React = require("react/addons");
 var TestUtils = React.addons.TestUtils;
 
@@ -22,43 +22,6 @@ describe("TaskTable", function () {
       var result = this.instance.getTaskUpdatedTimestamp(task);
       var lastStatus = task.statuses[task.statuses.length - 1];
       expect(result).toEqual(lastStatus.timestamp);
-    });
-  });
-
-  describe("#getSortFunction", function () {
-    beforeEach(function () {
-      this.sortFunction = this.instance.getSortFunction("name");
-      this.task = Tasks[0];
-    });
-
-    it("should return a function", function () {
-      expect(_.isFunction(this.sortFunction)).toEqual(true);
-    });
-
-    it("should return a number if prop is mem", function () {
-      var sortPropFunction = this.sortFunction("mem");
-      expect(typeof sortPropFunction(this.task)).toEqual("number");
-    });
-
-    it("should return a number if prop is cpus", function () {
-      var sortPropFunction = this.sortFunction("cpus");
-      expect(typeof sortPropFunction(this.task)).toEqual("number");
-    });
-
-    it("should the most recent date if prop is updated", function () {
-      var sortPropFunction = this.sortFunction("updated");
-      expect(sortPropFunction(this.task))
-        .toEqual(this.instance.getTaskUpdatedTimestamp(this.task));
-    });
-
-    it("should return a string if prop is name", function () {
-      var sortPropFunction = this.sortFunction("name");
-      expect(typeof sortPropFunction(this.task)).toEqual("string");
-    });
-
-    it("should return a string if prop is state", function () {
-      var sortPropFunction = this.sortFunction("state");
-      expect(typeof sortPropFunction(this.task)).toEqual("string");
     });
   });
 });
