ee68e0bf29e742b6ea02dfe69bdc763067002500
Kenny Tran <kenny8tran@gmail.com>
2015-09-21 10:48:21 -0700

    Add tests for task table

--- src/js/components/__tests__/TaskTable-test.js
+++ src/js/components/__tests__/TaskTable-test.js
@@ -10,8 +10,31 @@ const Tasks = require("./fixtures/MockTasks").tasks;
 
 describe("TaskTable", function () {
   beforeEach(function () {
+    this.parentRouter = {
+      getCurrentRoutes: function () {
+        return [{name: "home"}, {name: "dashboard"}, {name: "service-detail"}];
+      }
+    };
+
     this.instance = TestUtils.renderIntoDocument(
-      <TaskTable tasks={Tasks} />
+      <TaskTable tasks={Tasks} parentRouter={this.parentRouter} />
     );
+  });
+
+  describe("#getTaskPanelRoute", function () {
+    it("should be able to get the link to task detail", function () {
+      var result = this.instance.getTaskPanelRoute();
+
+      expect(result).toEqual("dashboard-task-panel");
+    });
+  });
+
+  describe("#handleTaskClick", function () {
+    it("should call transitionTo on parentRouter", function () {
+      this.parentRouter.transitionTo = jasmine.createSpy();
+      this.instance.handleTaskClick();
+
+      expect(this.parentRouter.transitionTo).toHaveBeenCalled();
+    });
   });
 });
