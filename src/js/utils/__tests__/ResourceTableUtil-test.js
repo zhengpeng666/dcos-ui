de58463801da8ff2db8270f7577b7b7f5ed88ab4
Kenny Tran <kenny8tran@gmail.com>
2015-10-06 10:35:12 -0700

    Add some more tests for ResourceTableUtil

--- src/js/utils/__tests__/ResourceTableUtil-test.js
+++ src/js/utils/__tests__/ResourceTableUtil-test.js
@@ -10,18 +10,14 @@ describe("ResourceTableUtil", function () {
   beforeEach(function () {
     this.getServiceHealth = MarathonStore.getServiceHealth;
     MarathonStore.getServiceHealth = function (prop) {
-      if (prop === "foo") {
-        return 0;
-      }
-      if (prop === "bar") {
-        return 1;
-      }
-    };
+      return this[prop].health;
+    }.bind(this);
 
     this.foo = {
       name: "foo",
       statuses: [{timestamp: 1}, {timestamp: 2}],
       updated: 0,
+      health: {key: "UNHEALTHY"},
       resources: {
         cpus: 100,
         mem: [{value: 2}, {value: 3}]
@@ -31,6 +27,7 @@ describe("ResourceTableUtil", function () {
       name: "bar",
       statuses: [{timestamp: 4}],
       updated: 1,
+      health: {key: "HEALTHY"},
       resources: {
         cpus: 5,
         mem: [{value: 0}, {value: 1}]
@@ -51,17 +48,36 @@ describe("ResourceTableUtil", function () {
       expect(_.isFunction(this.sortFunction)).toEqual(true);
     });
 
-    it("should the most recent timestamps when prop is updated", function () {
-      var sortPropFunction = this.sortFunction("updated");
-      expect(sortPropFunction(this.foo, this.bar))
-        .toEqual(2 - 4);
-    });
+    it("should return the most recent timestamps when prop is updated",
+      function () {
+        var sortPropFunction = this.sortFunction("updated");
+        expect(sortPropFunction(this.foo, this.bar))
+          .toEqual(2 - 4);
+      }
+    );
 
     it("should compare baseProp values", function () {
       var sortPropFunction = this.sortFunction("name");
 
       // "foo" > "bar" will equal true and compareValues returns 1
       expect(sortPropFunction(this.foo, this.bar)).toEqual(1);
+    });
+
+    it("should compare the health correctly", function () {
+      var sortPropFunction = this.sortFunction("health");
+      expect(sortPropFunction(this.foo, this.bar)).toEqual(-1);
+    });
+
+    it("should use the baseProp if health is the same", function () {
+      var prevHealth = this.bar.health.key;
+      this.bar.health.key = this.foo.health.key;
+      var sortPropFunction = this.sortFunction("health");
+
+      // Will compare with names now: "foo" > "bar" which will return 1
+      expect(sortPropFunction(this.foo, this.bar)).toEqual(1);
+
+      // Set it back to original health
+      this.bar.health.key = prevHealth;
     });
   });
 
@@ -73,6 +89,10 @@ describe("ResourceTableUtil", function () {
           return obj.resources[prop];
         }
       );
+    });
+
+    it("should return a function", function () {
+      expect(_.isFunction(this.sortFunction)).toEqual(true);
     });
 
     it("should compare resource values", function () {
