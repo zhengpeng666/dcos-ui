0d20ed48c2ba30006e280b9b270edc719d66df84
Michael Lunøe <michael.lunoe@gmail.com>
2015-10-06 11:16:04 -0700

    Revert "get most recent successful snapshot"

--- src/js/pages/DashboardPage.js
+++ src/js/pages/DashboardPage.js
@@ -22,6 +22,7 @@ import TaskSidePanel from "../components/TaskSidePanel";
 function getMesosState() {
   let states = MesosSummaryStore.get("states");
   let last = states.last();
+  let services = last.getServiceList();
 
   return {
     // Need clone, modifying in place will make update components check for
@@ -29,7 +30,7 @@ function getMesosState() {
     taskFailureRate: _.clone(MesosSummaryStore.get("taskFailureRate")),
     hostsCount: states.getActiveNodesByState(),
     refreshRate: Config.getRefreshRate(),
-    services: states.getActiveServices(),
+    services,
     usedResourcesStates: states.getResourceStatesForNodeIDs(),
     usedResources: last.getSlaveUsedResources(),
     totalResources: last.getSlaveTotalResources(),
