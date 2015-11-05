d8856c63ff928a265ba94166088c0cc72a7bcc97
John Furrow <johnf@mesosphere.io>
2015-11-04 16:58:56 -0800

    Add getTaskStatusIcon and getTaskStatusSlug

--- src/js/utils/TaskUtil.js
+++ src/js/utils/TaskUtil.js
@@ -1,8 +1,31 @@
+/*eslint-disable no-unused-vars*/
+const React = require("react/addons");
+/*eslint-enable no-unused-vars*/
+
 var TaskUtil = {
 
+  getTaskStatusSlug(task) {
+    return task.state.substring("TASK_".length).toLowerCase();
+  },
+
   getTaskStatusClassName(task) {
-    let taskStatus = task.state.substring("TASK_".length).toLowerCase();
+    let taskStatus = TaskUtil.getTaskStatusSlug(task);
     return `task-status-${taskStatus}`;
+  },
+
+  getTaskStatusIcon(task) {
+    let taskStatus = TaskUtil.getTaskStatusSlug(task);
+    let iconClassName = `icon icon-sprite icon-sprite-mini icon-${taskStatus}`;
+
+    // "staging", "finished", and "starting" all use their monochromatic colors.
+    if (taskStatus !== "staging" && taskStatus !== "finished" &&
+      taskStatus !== "starting") {
+      iconClassName += " icon-sprite-mini-color";
+    }
+
+    return (
+      <div className={iconClassName}></div>
+    );
   }
 
 };
