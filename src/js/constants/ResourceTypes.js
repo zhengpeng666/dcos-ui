b6ea5d4dd5cc10ec80e6e7d9741e7c85d534a3ec
John Furrow <johnf@mesosphere.io>
2015-11-06 14:42:21 -0800

    Fix color discrepencies

--- src/js/constants/ResourceTypes.js
+++ src/js/constants/ResourceTypes.js
@@ -1,7 +1,7 @@
 var ResourceTypes = {
   cpus: {label: "CPU", colorIndex: 0},
-  mem: {label: "Memory", colorIndex: 6},
-  disk: {label: "Disk", colorIndex: 3}
+  mem: {label: "Memory", colorIndex: 6}, //pink
+  disk: {label: "Disk", colorIndex: 3} //blue
 };
 
 module.exports = ResourceTypes;
