1d04028edefdcb013539f0f1b7fd10a11da2d2de
Kenny Tran <kenny8tran@gmail.com>
2015-11-10 16:32:03 -0800

    Basic pass at creating plugin API

---
+++ src/js/plugins/plugin.js
@@ -0,0 +1,52 @@
+import _ from "underscore";
+
+function addListener(hookName, listener, store) {
+  let listeners = store[hookName];
+
+  if (_.isArray(listeners)) {
+    listeners.push(listener);
+  } else {
+    store[hookName] = [listener];
+  }
+}
+
+const plugins = {
+  actions: {},
+
+  filters: {
+    renderingBanner: []
+  },
+
+  doAction(hookName) {
+    let listeners = this.actions[hookName];
+    let args = Array.prototype.slice.apply(arguments, 1);
+
+    if (_.isArray(listeners)) {
+      listeners.forEach(function (listener) {
+        listener.apply(this, args);
+      }, this);
+    }
+  },
+
+  addAction(hookName, listener) {
+    addListener(hookName, listener, this.actions);
+  },
+
+  applyFilter(hookName, arg) {
+    let listeners = this.filters[hookName];
+
+    if (_.isArray(listeners)) {
+      return listeners.reduce(function (total, listener) {
+        return listener.call(this, total);
+      }.bind(this), arg);
+    }
+
+    return null;
+  },
+
+  addFilter(hookName, listener) {
+    addListener(hookName, listener, this.filters);
+  }
+};
+
+export default plugins;
