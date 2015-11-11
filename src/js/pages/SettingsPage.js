9aa57d9d9872c8bdd3a819f1bda1931eb12db41a
Kenny Tran <kenny8tran@gmail.com>
2015-11-11 11:23:02 -0800

    Make icon work

--- src/js/pages/SettingsPage.js
+++ src/js/pages/SettingsPage.js
@@ -12,6 +12,6 @@ export default class SettingsPage extends React.Component {
 
 SettingsPage.routeConfig = {
   label: "Settings",
-  icon: "services",
+  icon: "resources-settings",
   matches: /^\/settings/
 };
