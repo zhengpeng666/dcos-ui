025d3942654c5cf1d376abd4750b0cdb70dc5b2d
Kenny Tran <kenny8tran@gmail.com>
2015-11-16 13:22:24 -0800

    Add fixture to template.js

--- src/js/config/Config.template.js
+++ src/js/config/Config.template.js
@@ -3,7 +3,17 @@ var ConfigDev = {
   analyticsKey: "39uhSEOoRHMw6cMR6st9tYXDbAL3JSaP",
   disableLoginModal: false,
   rootUrl: "http://dcos.local",
-  historyServer: "http://dcos.local"
+  historyServer: "http://dcos.local",
+  uiConfigurationFixture: {
+    uiConfiguration: {
+      plugins: {
+        banner: {},
+        tracking: {
+          enabled: true
+        }
+      }
+    }
+  }
 };
 
 module.exports = ConfigDev;
