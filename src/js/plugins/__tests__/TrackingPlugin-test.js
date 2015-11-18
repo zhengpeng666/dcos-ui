65042fcc085f3a1fcbb51d4d751f2ec9c9bf4b80
Kenny Tran <kenny8tran@gmail.com>
2015-11-18 15:07:34 -0800

    Test public API for TrackingPlugin

--- src/js/plugins/__tests__/TrackingPlugin-test.js
+++ src/js/plugins/__tests__/TrackingPlugin-test.js
@@ -7,10 +7,32 @@ jest.dontMock("../../events/ConfigActions");
 jest.dontMock("../../mixins/GetSetMixin");
 jest.dontMock("../../stores/ConfigStore");
 jest.dontMock("../../utils/Store");
+jest.dontMock("../../utils/DOMUtils");
+
+var TrackingPlugin = require("../TrackingPlugin");
+
+jest.setMock("../index", {
+  tracking: TrackingPlugin
+});
 
 var _ = require("underscore");
 
-var TrackingPlugin = require("../TrackingPlugin");
+var ConfigStore = require("../../stores/ConfigStore");
+var DOMUtils = require("../../utils/DOMUtils");
+var EventTypes = require("../../constants/EventTypes");
+var Plugins = require("../Plugins");
+
+function processConfigState(isEnabled) {
+  ConfigStore.processStateSuccess({
+    uiConfiguration: {
+      plugins: {
+        tracking: {
+          enabled: isEnabled
+        }
+      }
+    }
+  });
+}
 
 describe("TrackingPlugin", function () {
 
@@ -49,6 +71,54 @@ describe("TrackingPlugin", function () {
       expect(this.TrackingPlugin.configuration.enabled).toBeFalsy();
       this.TrackingPlugin.configure({enabled: true});
       expect(this.TrackingPlugin.configuration.enabled).toBeTruthy();
+    });
+  });
+
+  describe("Listeners", function () {
+    beforeEach(function () {
+      Plugins.initialize();
+      DOMUtils.appendScript = jasmine.createSpy();
+    });
+
+    describe("#pluginsConfigured", function () {
+
+      it("appends scripts to the document head if plugin enabled", function () {
+        processConfigState(true);
+        expect(DOMUtils.appendScript.callCount).toEqual(2);
+      });
+
+      it("does not append scripts if plugin disabled", function () {
+        processConfigState(false);
+        expect(DOMUtils.appendScript.callCount).toEqual(0);
+      });
+    });
+
+    describe("#openLoginModal", function () {
+      it("returns the value given to it if plugin enabled", function () {
+        processConfigState(true);
+        var result = Plugins.applyFilter("openLoginModal", "hello");
+        expect(result).toEqual("hello");
+      });
+
+      it("returns false if plugin disabled", function () {
+        processConfigState(false);
+        var result = Plugins.applyFilter("openLoginModal", "hello");
+        expect(result).toEqual(false);
+      });
+    });
+
+    describe("#footerButtonSet", function () {
+      it("returns the value given to it if plugin enabled", function () {
+        processConfigState(true);
+        var result = Plugins.applyFilter("footerButtonSet", "hello");
+        expect(result).toEqual("hello");
+      });
+
+      it("returns an empty array if plugin disabled", function () {
+        processConfigState(false);
+        var result = Plugins.applyFilter("footerButtonSet", "hello");
+        expect(result).toEqual([]);
+      });
     });
   });
 });
