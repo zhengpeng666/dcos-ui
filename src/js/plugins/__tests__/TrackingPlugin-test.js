e552ec4edf66fc8a9814e2feece13cfcc60354ad
Kenny Tran <kenny8tran@gmail.com>
2015-11-19 11:56:53 -0800

    Test and use #isEnabled

--- src/js/plugins/__tests__/TrackingPlugin-test.js
+++ src/js/plugins/__tests__/TrackingPlugin-test.js
@@ -44,32 +44,42 @@ describe("TrackingPlugin", function () {
       TrackingPlugin.initialize(this.Plugins);
     });
 
-    it("should add one action and two addFilters", function () {
-      expect(this.Plugins.addFilter.mock.calls.length).toEqual(2);
-      expect(this.Plugins.addAction.mock.calls.length).toEqual(1);
-    });
-
-    it("should call it with an event name and a callback", function () {
-      var calls = this.Plugins.addAction.mock.calls.concat(
-        this.Plugins.addFilter.mock.calls
+    it("should add one action and two filters", function () {
+      expect(this.Plugins.addAction.mock.calls[0]).toEqual(
+        ["pluginsConfigured", TrackingPlugin.pluginsConfigured]
       );
-
-      calls.forEach(function (call) {
-        expect(typeof call[0]).toEqual("string");
-        expect(typeof call[1]).toEqual("function");
-      });
+      expect(this.Plugins.addFilter.mock.calls[0]).toEqual(
+        ["openLoginModal", TrackingPlugin.openLoginModal]
+      );
+      expect(this.Plugins.addFilter.mock.calls[1]).toEqual(
+        ["footerButtonSet", TrackingPlugin.footerButtonSet]
+      );
     });
   });
 
-  describe("#configure", function () {
+  describe("Setting and changing configuration", function () {
     beforeEach(function () {
       this.TrackingPlugin = _.clone(TrackingPlugin);
+      this.TrackingPlugin.configuration = _.clone(TrackingPlugin.configuration);
     });
 
-    it("changes the plugin's configuration", function () {
-      expect(this.TrackingPlugin.configuration.enabled).toBeFalsy();
-      this.TrackingPlugin.configure({enabled: true});
-      expect(this.TrackingPlugin.configuration.enabled).toBeTruthy();
+    describe("#isEnabled", function () {
+      it("returns true if configured to be enabled", function () {
+        this.TrackingPlugin.configure({enabled: true});
+        expect(this.TrackingPlugin.isEnabled()).toBeTruthy();
+      });
+
+      it("defaults to false", function () {
+        expect(this.TrackingPlugin.isEnabled()).toBeFalsy();
+      });
+    });
+
+    describe("#configure", function () {
+      it("changes the plugin's configuration", function () {
+        expect(this.TrackingPlugin.isEnabled()).toBeFalsy();
+        this.TrackingPlugin.configure({enabled: true});
+        expect(this.TrackingPlugin.isEnabled()).toBeTruthy();
+      });
     });
   });
 
