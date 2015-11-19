287ebbe2d6a2525be35ccc5c3d87d20fa0dfd894
John Furrow <johnf@mesosphere.io>
2015-11-19 10:16:19 -0800

    Add tests for Core struct and CompositeState struct API

---
+++ src/js/structs/__tests__/CompositeState-test.js
@@ -0,0 +1,83 @@
+jest.autoMockOff();
+
+let CompositeState = require("../CompositeState");
+
+describe("Core", function () {
+
+  beforeEach(function() {
+    this.instance = new CompositeState();
+  });
+
+  describe("#constructor", function () {
+
+    it("populates data with the object it's given", function () {
+      this.instance = new CompositeState({foo: "bar"});
+      expect(this.instance.data).toEqual({foo: "bar"});
+    });
+
+  });
+
+  describe("#addState", function () {
+
+    it("adds an object to state", function () {
+      this.instance.addState({foo: "bar"});
+      expect(this.instance.data.state).toEqual({foo: "bar"});
+    });
+
+    it("merges old and new states", function () {
+      this.instance.addState({foo: "bar"});
+      this.instance.addState({baz: "qux"});
+      expect(this.instance.data.state).toEqual({foo: "bar", baz: "qux"});
+    });
+
+    it("merges old and new states, overwriting old with new", function () {
+      this.instance.addState({foo: "bar"});
+      this.instance.addState({baz: "qux"});
+      this.instance.addState({foo: "baz"});
+      expect(this.instance.data.state).toEqual({foo: "baz", baz: "qux"});
+    });
+
+  });
+
+  describe("#addMarathon", function () {
+
+    it("adds an object to marathon state", function () {
+      this.instance.addMarathon({foo: "bar"});
+      expect(this.instance.data.marathon).toEqual({foo: "bar"});
+    });
+
+    it("merges old and new marathon states", function () {
+      this.instance.addMarathon({foo: "bar"});
+      this.instance.addMarathon({baz: "quux"});
+      expect(this.instance.data.marathon).toEqual(
+        {foo: "bar", baz: "quux"}
+      );
+    });
+
+    it("merges old and new marathon states, overwriting old with new",
+      function () {
+      this.instance.addMarathon({foo: "bar"});
+      this.instance.addMarathon({baz: "quux"});
+      this.instance.addMarathon({foo: "baz", corge: "grault"});
+      expect(this.instance.data.marathon).toEqual(
+        {foo: "baz", baz: "quux", corge: "grault"}
+      );
+    });
+
+  });
+
+  describe("#getServiceList", function () {
+
+    it("returns a list with its current state and marathon data", function () {
+      this.instance.addMarathon({foo: "bar"});
+      this.instance.addState({baz: "qux"});
+
+      let serviceList = this.instance.getServiceList();
+
+      expect(serviceList.list[0].marathon).toEqual({foo: "bar"});
+      expect(serviceList.list[0].state).toEqual({baz: "qux"});
+    });
+
+  });
+
+});
---
+++ src/js/structs/__tests__/Core-test.js
@@ -0,0 +1,100 @@
+jest.autoMockOff();
+
+let Core = require("../Core");
+
+describe("Core", function () {
+
+  describe("#addState", function () {
+
+    it("adds an object to state", function () {
+      Core.addState({foo: "bar"});
+      expect(Core.compositeState.data.state).toEqual({foo: "bar"});
+    });
+
+    it("merges old and new states", function () {
+      Core.addState({baz: "qux"});
+      expect(Core.compositeState.data.state).toEqual({foo: "bar", baz: "qux"});
+    });
+
+    it("merges old and new states, overwriting old with new", function () {
+      Core.addState({foo: "baz"});
+      expect(Core.compositeState.data.state).toEqual({foo: "baz", baz: "qux"});
+    });
+
+  });
+
+  describe("#addMarathon", function () {
+
+    it("adds an object to marathon state", function () {
+      Core.addMarathon({foo: "bar"});
+      expect(Core.compositeState.data.marathon).toEqual({foo: "bar"});
+    });
+
+    it("merges old and new marathon states", function () {
+      Core.addMarathon({baz: "quux"});
+      expect(Core.compositeState.data.marathon).toEqual(
+        {foo: "bar", baz: "quux"}
+      );
+    });
+
+    it("merges old and new marathon states, overwriting old with new",
+      function () {
+      Core.addMarathon({foo: "baz", corge: "grault"});
+      expect(Core.compositeState.data.marathon).toEqual(
+        {foo: "baz", baz: "quux", corge: "grault"}
+      );
+    });
+
+  });
+
+  describe("#addSummary", function () {
+
+    it("adds a summary snapshot", function () {
+      Core.addSummary({
+        frameworks: [{foo: "bar"}],
+        slaves: [{bar: "baz"}],
+        cluster: "qux",
+        hostname: "corge"
+      });
+
+      expect(Core.summary.list[0].snapshot).toEqual({
+        frameworks: [{foo: "bar"}],
+        slaves: [{bar: "baz"}],
+        cluster: "qux",
+        hostname: "corge"
+      });
+    });
+
+    it("adds multiple summary snapshots", function () {
+      Core.addSummary({
+        frameworks: [{foo: "bar"}],
+        slaves: [{bar: "baz"}],
+        cluster: "qux",
+        hostname: "corge"
+      });
+      Core.addSummary({
+        frameworks: [{foo: "bar"}],
+        slaves: [{bar: "baz"}],
+        cluster: "qux",
+        hostname: "corge"
+      });
+
+      expect(Core.summary.list.length).toEqual(3);
+    });
+
+  });
+
+  describe("#getLatest", function () {
+
+    it("returns state and marathon summaries merged into one object",
+      function() {
+        expect(Core.getLatest()).toEqual({
+          foo: "baz",
+          baz: "quux",
+          corge: "grault"
+        });
+    });
+
+  });
+
+});
