70f631e5a6940d7c1419a363ab5a06b47976fc38
John Furrow <johnf@mesosphere.io>
2015-11-23 20:48:56 -0800

    Pass an object to GroupsList instead of array

--- src/js/structs/User.js
+++ src/js/structs/User.js
@@ -3,7 +3,9 @@ import Item from "./Item";
 
 export default class User extends Item {
   getGroups() {
-    return new GroupsList(this.get("groups"));
+    return new GroupsList({
+      items: this.get("groups")
+    });
   }
 
   getPermissions() {
