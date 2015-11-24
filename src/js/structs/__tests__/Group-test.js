27b66066a95044e665cc7322a4f3a0aadff7c131
John Furrow <johnf@mesosphere.io>
2015-11-24 13:30:39 -0800

    Prevent mutation of structs

--- src/js/structs/__tests__/Group-test.js
+++ src/js/structs/__tests__/Group-test.js
@@ -1,3 +1,4 @@
+jest.dontMock("underscore");
 jest.dontMock("../Group");
 jest.dontMock("../Item");
 jest.dontMock("../List");
@@ -5,6 +6,7 @@ jest.dontMock("../User");
 jest.dontMock("../UsersList");
 jest.dontMock("../../utils/Util");
 
+let _ = require("underscore");
 let Group = require("../Group");
 let groupFixture = require("./fixtures/groupData");
 let UsersList = require("../UsersList");
@@ -13,12 +15,14 @@ describe("Group", function () {
 
   beforeEach(function () {
     this.instance = new Group(groupFixture);
+    this.groupFixture = _.clone(groupFixture);
   });
 
   describe("#getPermissions", function () {
 
     it("returns the permissions it was given", function () {
-      expect(this.instance.getPermissions()).toEqual(groupFixture.permissions);
+      expect(this.instance.getPermissions())
+        .toEqual(this.groupFixture.permissions);
     });
 
   });
@@ -33,13 +37,16 @@ describe("Group", function () {
     it("returns a UsersList with the number of items we provided",
       function () {
       let users = this.instance.getUsers();
-      expect(users.list.length).toEqual(groupFixture.users.length);
+      expect(users.list.length)
+        .toEqual(this.groupFixture.users.length);
     });
 
     it("returns a UsersList with the data we provided", function () {
       let users = this.instance.getUsers();
-      expect(users.list[0].uid).toEqual(groupFixture.users[0].user.uid);
-      expect(users.list[1].uid).toEqual(groupFixture.users[1].user.uid);
+      expect(users.list[0].uid)
+        .toEqual(this.groupFixture.users[0].user.uid);
+      expect(users.list[1].uid)
+        .toEqual(this.groupFixture.users[1].user.uid);
     });
 
   });
--- src/js/structs/__tests__/User-test.js
+++ src/js/structs/__tests__/User-test.js
@@ -1,3 +1,4 @@
+jest.dontMock("underscore");
 jest.dontMock("../Group");
 jest.dontMock("../GroupsList");
 jest.dontMock("../Item");
@@ -6,6 +7,7 @@ jest.dontMock("../User");
 jest.dontMock("../UsersList");
 jest.dontMock("../../utils/Util");
 
+let _ = require("underscore");
 let GroupsList = require("../GroupsList");
 let User = require("../User");
 let userFixture = require("./fixtures/userData");
@@ -14,12 +16,14 @@ describe("User", function () {
 
   beforeEach(function () {
     this.instance = new User(userFixture);
+    this.userFixture = _.clone(userFixture);
   });
 
   describe("#getPermissions", function () {
 
     it("returns the permissions it was given", function () {
-      expect(this.instance.getPermissions()).toEqual(userFixture.permissions);
+      expect(this.instance.getPermissions())
+        .toEqual(this.userFixture.permissions);
     });
 
   });
@@ -34,13 +38,16 @@ describe("User", function () {
     it("returns a GroupsList with the number of items we provided",
       function () {
       let groups = this.instance.getGroups();
-      expect(groups.list.length).toEqual(userFixture.groups.length);
+      expect(groups.list.length)
+        .toEqual(this.userFixture.groups.length);
     });
 
     it("returns a GroupsList with the data we provided", function () {
       let groups = this.instance.getGroups();
-      expect(groups.list[0].gid).toEqual(userFixture.groups[0].group.gid);
-      expect(groups.list[1].gid).toEqual(userFixture.groups[1].group.gid);
+      expect(groups.list[0].gid)
+        .toEqual(this.userFixture.groups[0].group.gid);
+      expect(groups.list[1].gid)
+        .toEqual(this.userFixture.groups[1].group.gid);
     });
 
   });
