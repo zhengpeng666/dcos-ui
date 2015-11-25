f0e46129a5d4f0a33800cf8625859dcf10885dbd
John Furrow <johnf@mesosphere.io>
2015-11-25 08:41:56 -0800

    Add more testsgit add .git add .1

--- src/js/stores/__tests__/GroupsStore-test.js
+++ src/js/stores/__tests__/GroupsStore-test.js
@@ -13,6 +13,8 @@ jest.dontMock("../../utils/Util");
 jest.dontMock("../../../../tests/_fixtures/acl/groups-unicode.json");
 
 let _ = require("underscore");
+let ActionTypes = require("../../constants/ActionTypes");
+let AppDispatcher = require("../../events/AppDispatcher");
 let Config = require("../../config/Config");
 let groupsFixture = require("../../../../tests/_fixtures/acl/groups-unicode.json");
 let GroupsList = require("../../structs/GroupsList");
@@ -46,5 +48,20 @@ describe("GroupsStore", function () {
     let groups = GroupsStore.get("groups").getItems();
     expect(groups.length).toEqual(this.groupsFixture.length);
   });
+
+  describe("dispatcher", function() {
+
+    it("stores groups when event is dispatched", function () {
+      AppDispatcher.handleServerAction({
+        type: ActionTypes.REQUEST_ACL_GROUPS_SUCCESS,
+        data: [{gid: "foo", bar: "baz"}]
+      })
+
+      let groups = GroupsStore.get("groups").list;
+      expect(groups[0].gid).toEqual("foo");
+      expect(groups[0].bar).toEqual("baz");
+    });
+
+  })
 
 });
