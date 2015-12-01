f6e58afca4e91ffd9a2be00a54b52054231a2ae6
Christine Sun <christine.sun@mesosphere.io>
2015-12-01 15:03:34 -0800

    create user side panel header info

--- src/js/components/UserSidePanelContents.js
+++ src/js/components/UserSidePanelContents.js
@@ -2,15 +2,112 @@
 import React from "react";
 /*eslint-enable no-unused-vars*/
 
-// import ACLUserStore from "../stores/ACLUserStore";
+import ACLUserStore from "../stores/ACLUserStore";
+import EventTypes from "../constants/EventTypes";
+import MesosSummaryStore from "../stores/MesosSummaryStore";
 import SidePanelContents from "./SidePanelContents";
+
+const METHODS_TO_BIND = [
+  "handleUserDetails",
+  "onMesosStateChange"
+];
 
 export default class UserSidePanelContents extends SidePanelContents {
 
+    constructor() {
+      super();
+
+      this.state = {
+        user: null
+      };
+
+      METHODS_TO_BIND.forEach(function (method) {
+        this[method] = this[method].bind(this);
+      }, this);
+    }
+
+    componentDidMount() {
+      ACLUserStore.addChangeListener(
+        EventTypes.ACL_USER_DETAILS_FETCHED_SUCCESS,
+        this.handleUserDetails
+      );
+
+      ACLUserStore.fetchUserWithDetails(this.props.itemID);
+
+      MesosSummaryStore.addChangeListener(
+        EventTypes.MESOS_SUMMARY_CHANGE,
+        this.onMesosStateChange
+      );
+    }
+
+    componentWillUnmount() {
+      ACLUserStore.removeChangeListener(
+        EventTypes.ACL_USER_DETAILS_FETCHED_SUCCESS,
+        this.handleUserDetails
+      );
+
+      this.removeMesosSummaryListener();
+    }
+
+    handleUserDetails() {
+      this.setState({
+        user: ACLUserStore.getUser(this.props.itemID)
+      });
+    }
+
+    onMesosStateChange() {
+      this.removeMesosSummaryListener();
+      this.forceUpdate();
+    }
+
+    removeMesosSummaryListener() {
+      MesosSummaryStore.removeChangeListener(
+        EventTypes.MESOS_SUMMARY_CHANGE,
+        this.onMesosStateChange
+      );
+    }
+
+    getUserInfo() {
+      let user = this.state.user;
+      let imageTag = (
+        <div className="side-panel-icon icon icon-large icon-image-container icon-user-container">
+          image here
+        </div>
+      );
+
+      return (
+        <div className="side-panel-content-header-details flex-box
+          flex-box-align-vertical-center">
+          {imageTag}
+          <div>
+            <h1 className="side-panel-content-header-label flush">
+              {user.uid}
+            </h1>
+            <div>
+              {this.getSubHeader(user)}
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    getSubHeader(user) {
+      let groupCount = user.getGroupCount();
+      let serviceCount = user.getPermissionCount();
+      let groupLabel = "group" + (groupCount === 1 ? "" : "s");
+      let serviceLabel = "Service" + (serviceCount === 1 ? "" : "s");
+
+      return (
+        <div>
+          {serviceCount} {serviceLabel}, Member of {groupCount} {groupLabel}
+        </div>
+      );
+    }
+
     render() {
-      let userID = this.props.itemID;
-      // let userDetails = ACLUserStore.fetchUserWithDetails(userID);
-      console.log(userID);
+      if (this.state.user == null) {
+        return (<div />);
+      }
 
       return (
         <div className="flex-container-col">
@@ -20,12 +117,8 @@ export default class UserSidePanelContents extends SidePanelContents {
               side-panel-content-header container container-pod
               container-fluid container-pod-divider-bottom
               container-pod-divider-bottom-align-right flush-bottom">
-            {"basic info"}
-            <ul className="tabs list-inline flush-bottom">
-              {"tabs"}
-            </ul>
+            {this.getUserInfo()}
           </div>
-          {"tab view"}
         </div>
       );
     }
--- src/js/structs/User.js
+++ src/js/structs/User.js
@@ -2,6 +2,35 @@ import GroupsList from "./GroupsList";
 import Item from "./Item";
 
 export default class User extends Item {
+
+  _uniquePermissions() {
+    let permissions = this.getPermissions();
+    let uniquePermissions = [];
+    let aclUrls = new Set();
+
+    permissions.direct.forEach(
+      (service) => {
+        let url = service.aclurl;
+        if (!aclUrls.has(url)) {
+          uniquePermissions.push(service);
+          aclUrls.add(url);
+        }
+      }
+    );
+
+    permissions.groups.forEach(
+      (service) => {
+        let url = service.aclurl;
+        if (!aclUrls.has(url)) {
+          uniquePermissions.push(service);
+          aclUrls.add(url);
+        }
+      }
+    );
+
+    return uniquePermissions;
+  }
+
   getGroups() {
     let groups = this.get("groups");
     let items = groups.map(function (groupMembership) {
@@ -10,7 +39,15 @@ export default class User extends Item {
     return new GroupsList({items});
   }
 
+  getGroupCount() {
+    return this.getGroups().getItems().length;
+  }
+
   getPermissions() {
     return this.get("permissions");
+  }
+
+  getPermissionCount() {
+    return this._uniquePermissions().length;
   }
 }
