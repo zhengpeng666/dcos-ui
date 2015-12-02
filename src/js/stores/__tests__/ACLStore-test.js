2c60c353b0a4dd0cb67e5a4411827e89f9ca2cc4
mlunoe <michael.lunoe@gmail.com>
2015-12-01 17:51:50 -0800

    Adding store tests

---
+++ src/js/stores/__tests__/ACLStore-test.js
@@ -0,0 +1,272 @@
+jest.dontMock("../ACLStore");
+jest.dontMock("../../config/Config");
+jest.dontMock("../../events/AppDispatcher");
+jest.dontMock("../../events/ACLGroupsActions");
+jest.dontMock("../../mixins/GetSetMixin");
+jest.dontMock("../../structs/Item");
+jest.dontMock("../../structs/ACLList");
+jest.dontMock("../../structs/Item");
+jest.dontMock("../../structs/List");
+jest.dontMock("../../utils/RequestUtil");
+jest.dontMock("../../utils/Store");
+jest.dontMock("../../utils/Util");
+jest.dontMock("../../../../tests/_fixtures/acl/acls-unicode.json");
+
+var _ = require("underscore");
+
+var ACLList = require("../../structs/ACLList");
+var ACLStore = require("../ACLStore");
+var ActionTypes = require("../../constants/ActionTypes");
+var AppDispatcher = require("../../events/AppDispatcher");
+var EventTypes = require("../../constants/EventTypes");
+var RequestUtil = require("../../utils/RequestUtil");
+
+var aclsFixture = require("../../../../tests/_fixtures/acl/acls-unicode.json");
+
+describe("ACLStore", function () {
+
+  describe("#fetchACLsForResource", function () {
+
+    beforeEach(function () {
+      this.requestFn = RequestUtil.json;
+      RequestUtil.json = function (handlers) {
+        handlers.success(aclsFixture);
+      };
+      this.aclsFixture = _.clone(aclsFixture);
+    });
+
+    afterEach(function () {
+      RequestUtil.json = this.requestFn;
+    });
+
+    it("should return an instance of ACLList", function () {
+      ACLStore.fetchACLsForResource("service");
+      var services = ACLStore.getACLsForType("services");
+      expect(services instanceof ACLList).toBeTruthy();
+    });
+
+    it("should return all of the services it was given", function () {
+      ACLStore.fetchACLsForResource("service");
+      var services = ACLStore.getACLsForType("services").getItems();
+      expect(services.length).toEqual(this.aclsFixture.length);
+    });
+
+  });
+
+  describe("dispatcher", function () {
+
+    describe("ACLs", function () {
+
+      it("stores services when event is dispatched", function () {
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_RESOURCE_ACLS_SUCCESS,
+          data: {response: [{rid: "foo", bar: "baz"}], type: "service"}
+        });
+
+        var services = ACLStore.getACLsForType("services").getItems();
+        expect(services[0].get("rid")).toEqual("foo");
+        expect(services[0].get("bar")).toEqual("baz");
+      });
+
+      it("dispatches the correct event upon success", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(EventTypes.ACL_RESOURCE_ACLS_CHANGE, mockedFn);
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_RESOURCE_ACLS_SUCCESS,
+          data: {response: [{rid: "foo", bar: "baz"}], type: "service"}
+        });
+
+        expect(mockedFn.mock.calls.length).toEqual(1);
+      });
+
+      it("dispatches the correct event upon error", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_RESOURCE_ACLS_REQUEST_ERROR,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_RESOURCE_ACLS_ERROR,
+          data: {error: "foo", type: "service"}
+        });
+
+        expect(mockedFn.mock.calls.length).toEqual(1);
+      });
+
+    });
+
+    describe("Grant User action", function () {
+
+      it("dispatches the correct event upon success", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_USER_GRANT_ACTION_CHANGE,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_USER_GRANT_ACTION_SUCCESS,
+          data: {userID: "foo", action: "access", resourceID: "marathon"}
+        });
+
+        expect(mockedFn.mock.calls[0][0])
+          .toEqual({userID: "foo", action: "access", resourceID: "marathon"});
+      });
+
+      it("dispatches the correct event upon error", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_USER_GRANT_ACTION_REQUEST_ERROR,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_USER_GRANT_ACTION_ERROR,
+          data: {
+            userID: "foo",
+            action: "access",
+            resourceID: "marathon",
+            error: "bar"
+          }
+        });
+
+        expect(mockedFn.mock.calls[0][0]).toEqual({
+          userID: "foo",
+          action: "access",
+          resourceID: "marathon",
+          error: "bar"
+        });
+      });
+
+    });
+
+    describe("Revoke User action", function () {
+
+      it("dispatches the correct event upon success", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_USER_REVOKE_ACTION_CHANGE,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_USER_REVOKE_ACTION_SUCCESS,
+          data: {userID: "foo", action: "access", resourceID: "marathon"}
+        });
+
+        expect(mockedFn.mock.calls[0][0])
+          .toEqual({userID: "foo", action: "access", resourceID: "marathon"});
+      });
+
+      it("dispatches the correct event upon error", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_USER_REVOKE_ACTION_REQUEST_ERROR,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_USER_REVOKE_ACTION_ERROR,
+          data: {
+            userID: "foo",
+            action: "access",
+            resourceID: "marathon",
+            error: "bar"
+          }
+        });
+
+        expect(mockedFn.mock.calls[0][0]).toEqual({
+          userID: "foo",
+          action: "access",
+          resourceID: "marathon",
+          error: "bar"
+        });
+      });
+
+    });
+
+    describe("Grant Group action", function () {
+
+      it("dispatches the correct event upon success", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_GROUP_GRANT_ACTION_CHANGE,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_GROUP_GRANT_ACTION_SUCCESS,
+          data: {groupID: "foo", action: "access", resourceID: "marathon"}
+        });
+
+        expect(mockedFn.mock.calls[0][0])
+          .toEqual({groupID: "foo", action: "access", resourceID: "marathon"});
+      });
+
+      it("dispatches the correct event upon error", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_GROUP_GRANT_ACTION_REQUEST_ERROR,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_GROUP_GRANT_ACTION_ERROR,
+          data: {
+            groupID: "foo",
+            action: "access",
+            resourceID: "marathon",
+            error: "bar"
+          }
+        });
+
+        expect(mockedFn.mock.calls[0][0]).toEqual({
+          groupID: "foo",
+          action: "access",
+          resourceID: "marathon",
+          error: "bar"
+        });
+      });
+
+    });
+
+    describe("Revoke Group action", function () {
+
+      it("dispatches the correct event upon success", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_GROUP_REVOKE_ACTION_CHANGE,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_GROUP_REVOKE_ACTION_SUCCESS,
+          data: {groupID: "foo", action: "access", resourceID: "marathon"}
+        });
+
+        expect(mockedFn.mock.calls[0][0])
+          .toEqual({groupID: "foo", action: "access", resourceID: "marathon"});
+      });
+
+      it("dispatches the correct event upon error", function () {
+        var mockedFn = jest.genMockFunction();
+        ACLStore.addChangeListener(
+          EventTypes.ACL_GROUP_REVOKE_ACTION_REQUEST_ERROR,
+          mockedFn
+        );
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_ACL_GROUP_REVOKE_ACTION_ERROR,
+          data: {
+            groupID: "foo",
+            action: "access",
+            resourceID: "marathon",
+            error: "bar"
+          }
+        });
+
+        expect(mockedFn.mock.calls[0][0]).toEqual({
+          groupID: "foo",
+          action: "access",
+          resourceID: "marathon",
+          error: "bar"
+        });
+      });
+
+    });
+
+  });
+
+});
