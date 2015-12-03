df1f384e80a855e776638705319b0af6b5df61d3
Christine Sun <christine.sun@mesosphere.io>
2015-12-03 13:31:09 -0800

    TEST

--- src/js/components/__tests__/UserSidePanelContents-test.js
+++ src/js/components/__tests__/UserSidePanelContents-test.js
@@ -4,41 +4,107 @@ jest.dontMock("../../events/MesosSummaryActions");
 jest.dontMock("../../stores/MesosSummaryStore");
 jest.dontMock("../../events/ACLUsersActions");
 jest.dontMock("../../stores/ACLUserStore");
+jest.dontMock("../../constants/EventTypes");
 jest.dontMock("../../mixins/GetSetMixin");
-jest.dontMock("./RequestErrorMsg");
+jest.dontMock("../../mixins/InternalStorageMixin");
+jest.dontMock("../../mixins/StoreMixin");
+jest.dontMock("../../mixins/TabsMixin");
+jest.dontMock("../RequestErrorMsg");
+jest.dontMock("../../utils/JestUtil");
 jest.dontMock("../../utils/MesosSummaryUtil");
 jest.dontMock("../../utils/StringUtil");
 jest.dontMock("../../utils/Store");
 jest.dontMock("../../utils/Util");
+jest.dontMock("../../structs/User");
 
-import React from "react";
-import TestUtils from "react-addons";
-// var TestUtils = React.addons.TestUtils;
+var React = require("react/addons");
+var TestUtils = React.addons.TestUtils;
 
-import ACLUserStore from "../stores/ACLUserStore";
-import MesosSummaryStore from "../stores/MesosSummaryStore";
-import RequestErrorMsg from "./RequestErrorMsg";
-import SidePanelContents from "./SidePanelContents";
-import StringUtil from "../utils/StringUtil";
-import UserSidePanelContents from "../UserSidePanelContents";
+var ACLUserStore = require("../../stores/ACLUserStore");
+var JestUtil = require("../../utils/JestUtil");
+var MesosSummaryStore = require("../../stores/MesosSummaryStore");
+var EventTypes = require("../../constants/EventTypes");
+var UserSidePanelContents = require("../UserSidePanelContents");
+var User = require("../../structs/User");
+
+var userDetailsFixture =
+  require("json!../../../tests/_fixtures/acl/user-with-details.json");
 
 describe("UserSidePanelContents", function () {
+
   beforeEach(function () {
-    //
+    this.summaryGet = MesosSummaryStore.get;
+    this.userStoreGetUser = ACLUserStore.getUser;
+
+    MesosSummaryStore.get = function (status) {
+      if (status === "statesProcessed") {
+        return true;
+      }
+    };
+
+    ACLUserStore.getUser = function (userID) {
+      if (userID === "unicode") {
+        return new User(userDetailsFixture);
+      }
+    };
+  });
+
+  afterEach(function () {
+    MesosSummaryStore.get = this.summaryGet;
+    ACLUserStore.getUser = this.userStoreGetUser;
   });
 
   describe("#render", function () {
-    beforeEach(function () {
-      //
+
+    it("should return error message if fetch error was received", function () {
+      var userID = "unicode";
+
+      var instance = TestUtils.renderIntoDocument(
+        <UserSidePanelContents
+          itemID={userID}/>
+      );
+
+      ACLUserStore.emit(EventTypes.ACL_USER_DETAILS_FETCHED_ERROR, userID);
+
+      var text = JestUtil.renderAndFindTag(instance.render(), "h3");
+      expect(text.getDOMNode().textContent)
+        .toEqual("Cannot Connect With The Server");
     });
 
-    it("should return error component if fetch error was received", function () {
+    it("should show loading screen if still waiting on Store", function () {
+      MesosSummaryStore.get = function (status) {
+        if (status === "statesProcessed") {
+          return false;
+        }
+      };
+      var userID = "unicode";
+
       var instance = TestUtils.renderIntoDocument(
-        <UserSidePanelContents open={true} />
+        <UserSidePanelContents
+          itemID={userID}/>
       );
-      expect(TestUtils.isElementOfType(instance.render(), RequestErrorMsg)).toEqual(null);
+
+      var loading = TestUtils.scryRenderedDOMComponentsWithClass(
+        instance.render(),
+        "ball-scale"
+      );
+
+      expect(loading).toEqual({});
     });
+
+    it("should not return error message or loading screen if user is found",
+      function () {
+        var userID = "unicode";
+
+        var instance = TestUtils.renderIntoDocument(
+          <UserSidePanelContents
+            itemID={userID}/>
+        );
+
+        var text = JestUtil.renderAndFindTag(instance.render(), "h1");
+        expect(text.getDOMNode().textContent).toEqual("qüis");
+      }
+    );
 
   });
-
 });
