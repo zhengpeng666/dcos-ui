ed64dace75b57f3d6e68e526a09fd2bcd9823ee5
Christine Sun <christine.sun@mesosphere.io>
2015-12-04 16:45:44 -0800

    add delete user modal to sidepanel

--- src/js/components/SidePanels.js
+++ src/js/components/SidePanels.js
@@ -1,21 +1,27 @@
 import _ from "underscore";
 import React from "react/addons";
-import {SidePanel} from "reactjs-components";
+import {Confirm, SidePanel} from "reactjs-components";
 
+import ACLUserStore from "../stores/ACLUserStore";
+import GroupSidePanelContents from "./GroupSidePanelContents";
 import HistoryStore from "../stores/HistoryStore";
 import MesosSummaryStore from "../stores/MesosSummaryStore";
 import NodeSidePanelContents from "./NodeSidePanelContents";
 import ServiceSidePanelContents from "./ServiceSidePanelContents";
+import StoreMixin from "../mixins/StoreMixin";
 import StringUtil from "../utils/StringUtil";
 import TaskSidePanelContents from "./TaskSidePanelContents";
 import UserSidePanelContents from "./UserSidePanelContents";
-import GroupSidePanelContents from "./GroupSidePanelContents";
+import Util from "../utils/Util";
 
 const METHODS_TO_BIND = [
-  "handlePanelClose"
+  "handleDeleteModalOpen",
+  "handleDeleteCancel",
+  "handlePanelClose",
+  "handleDeleteUser"
 ];
 
-export default class SidePanels extends React.Component {
+export default class SidePanels extends Util.mixin(StoreMixin) {
   constructor() {
     super();
 
@@ -23,7 +29,44 @@ export default class SidePanels extends React.Component {
       this[method] = this[method].bind(this);
     }, this);
 
-    this.state = {};
+    this.state = {
+      deleteUpdateError: null,
+      itemID: null,
+      openDeleteConfirmation: false,
+      pendingRequest: false
+    };
+
+    this.store_listeners = [
+      {
+        name: "user",
+        events: [
+          "deleteSuccess",
+          "deleteError"
+        ]
+      }
+    ];
+  }
+
+  handleDeleteCancel() {
+    this.setState({
+      itemID: null,
+      openDeleteConfirmation: false
+    });
+  }
+
+  handleDeleteModalOpen() {
+    this.setState({
+      itemID: this.props.params.userID,
+      deleteUpdateError: null,
+      openDeleteConfirmation: true
+    });
+  }
+
+  handleDeleteUser() {
+    ACLUserStore.deleteUser(this.state.itemID);
+    this.setState({
+      pendingRequest: true
+    });
   }
 
   handlePanelClose(closeInfo) {
@@ -51,7 +94,26 @@ export default class SidePanels extends React.Component {
     ) && MesosSummaryStore.get("statesProcessed");
   }
 
-  getHeader() {
+  getDeleteModalContent() {
+    let error = null;
+
+    if (this.state.deleteUpdateError != null) {
+      error = (
+        <p className="text-error-state">{this.state.deleteUpdateError}</p>
+      );
+    }
+
+    let user = ACLUserStore.getUser(this.state.itemID);
+    return (
+      <div className="container-pod text-align-center">
+        <h4>Are you sure?</h4>
+        <p>{`${StringUtil.capitalize(user.description)} will be deleted.`}</p>
+        {error}
+      </div>
+    );
+  }
+
+  getHeader(userID) {
     let text = "back";
     let prevPage = HistoryStore.getHistoryAt(-1);
 
@@ -69,18 +131,41 @@ export default class SidePanels extends React.Component {
     }
 
     return (
-      <div className="side-panel-header-actions
-        side-panel-header-actions-primary">
-        <span className="side-panel-header-action"
-          onClick={this.handlePanelClose}>
-          <i className={`icon icon-sprite
-            icon-sprite-small
-            icon-${text}
-            icon-sprite-small-white`}></i>
-          {StringUtil.capitalize(text)}
-        </span>
+      <div className="side-panel-header-container">
+        <div className="side-panel-header-actions
+          side-panel-header-actions-primary">
+
+          <span className="side-panel-header-action"
+            onClick={this.handlePanelClose}>
+            <i className={`icon icon-sprite
+              icon-sprite-small
+              icon-${text}
+              icon-sprite-small-white`}></i>
+            {StringUtil.capitalize(text)}
+          </span>
+
+        </div>
+
+        <div className="side-panel-header-actions
+          side-panel-header-actions-secondary">
+          {this.getHeaderDelete(userID)}
+        </div>
+
       </div>
     );
+  }
+
+  getHeaderDelete(userID) {
+    if (userID != null) {
+      return (
+        <span className="side-panel-header-action"
+          onClick={this.handleDeleteModalOpen}>
+          Delete
+        </span>
+      );
+    }
+
+    return null;
   }
 
   getContents(ids) {
@@ -133,6 +218,21 @@ export default class SidePanels extends React.Component {
     return null;
   }
 
+  onUserStoreDeleteError(userID, error) {
+    this.setState({
+      pendingRequest: false,
+      deleteUpdateError: error
+    });
+  }
+
+  onUserStoreDeleteSuccess() {
+    this.setState({
+      itemID: null,
+      openDeleteConfirmation: false,
+      pendingRequest: false
+    });
+  }
+
   render() {
     let props = this.props;
     let params = props.params;
@@ -144,16 +244,28 @@ export default class SidePanels extends React.Component {
     let groupID = params.groupID;
 
     return (
-      <SidePanel className="side-panel-detail"
-        header={this.getHeader()}
-        headerContainerClass="side-panel-header-container container
-          container-fluid container-fluid-narrow container-pod
-          container-pod-short"
-        bodyClass="side-panel-content flex-container-col"
-        onClose={this.handlePanelClose}
-        open={this.isOpen()}>
-        {this.getContents({nodeID, serviceName, taskID, userID, groupID})}
-      </SidePanel>
+      <div>
+        <SidePanel className="side-panel-detail"
+          header={this.getHeader(userID, groupID)}
+          headerContainerClass="container
+            container-fluid container-fluid-narrow container-pod
+            container-pod-short"
+          bodyClass="side-panel-content flex-container-col"
+          onClose={this.handlePanelClose}
+          open={this.isOpen()}>
+          {this.getContents({nodeID, serviceName, taskID, userID, groupID})}
+        </SidePanel>
+        <Confirm
+          closeByBackdropClick={true}
+          disabled={this.state.pendingRequest}
+          footerClass="modal-footer container container-pod container-pod-fluid"
+          open={this.state.openDeleteConfirmation}
+          onClose={this.handleDeleteCancel}
+          leftButtonCallback={this.handleDeleteCancel}
+          rightButtonCallback={this.handleDeleteUser}>
+          {this.getDeleteModalContent()}
+        </Confirm>
+      </div>
     );
   }
 }
