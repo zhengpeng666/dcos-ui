3edc077b79e59922dc457d8226a77688fb1de00a
Christine Sun <christine.sun@mesosphere.io>
2015-12-07 16:01:39 -0800

    fix tests

--- src/js/components/__tests__/SidePanels-test.js
+++ src/js/components/__tests__/SidePanels-test.js
@@ -2,6 +2,7 @@ jest.dontMock("../SidePanels");
 jest.dontMock("../../mixins/InternalStorageMixin");
 jest.dontMock("../../mixins/TabsMixin");
 jest.dontMock("../../mixins/GetSetMixin");
+jest.dontMock("../../stores/ACLUserStore");
 jest.dontMock("../../stores/MesosSummaryStore");
 jest.dontMock("../../stores/MesosStateStore");
 jest.dontMock("../../stores/MarathonStore");
@@ -9,9 +10,11 @@ jest.dontMock("../../utils/MesosSummaryUtil");
 jest.dontMock("../../events/MesosSummaryActions");
 jest.dontMock("../../events/MarathonActions");
 jest.dontMock("../SidePanelContents");
+jest.dontMock("../GroupSidePanelContents");
 jest.dontMock("../NodeSidePanelContents");
 jest.dontMock("../ServiceSidePanelContents");
 jest.dontMock("../TaskSidePanelContents");
+jest.dontMock("../UserSidePanelContents");
 jest.dontMock("../../utils/Store");
 jest.dontMock("../../utils/Util");
 jest.dontMock("../../utils/RequestUtil");
@@ -21,15 +24,19 @@ var React = require("react/addons");
 var TestUtils = React.addons.TestUtils;
 
 var MesosSummaryActions = require("../../events/MesosSummaryActions");
+var ACLUserStore = require("../../stores/ACLUserStore");
 var MesosSummaryStore = require("../../stores/MesosSummaryStore");
 var NodeSidePanelContents = require("../NodeSidePanelContents");
+var GroupSidePanelContents = require("../GroupSidePanelContents");
 var TaskSidePanelContents = require("../TaskSidePanelContents");
 var ServiceSidePanelContents = require("../ServiceSidePanelContents");
+var UserSidePanelContents = require("../UserSidePanelContents");
 var SidePanels = require("../SidePanels");
 
 describe("SidePanels", function () {
   beforeEach(function () {
     this.fetchSummary = MesosSummaryActions.fetchSummary;
+    this.userStore = ACLUserStore.getUser;
 
     MesosSummaryActions.fetchSummary = function () {
       return null;
@@ -38,16 +45,32 @@ describe("SidePanels", function () {
     MesosSummaryStore.get = function () {
       return true;
     };
+
+    ACLUserStore.getUser = function () {
+      return {
+        "uid": "user",
+        "url": "/users/user",
+        "description": "user description"
+      };
+    };
+
     MesosSummaryStore.init();
   });
 
   afterEach(function () {
     MesosSummaryActions.fetchSummary = this.fetchSummary;
+    ACLUserStore.getUser = this.userStore;
   });
 
   describe("#isOpen", function () {
     beforeEach(function () {
-      this.params = {nodeID: null, serviceName: null, taskID: null};
+      this.params = {
+        nodeID: null,
+        serviceName: null,
+        taskID: null,
+        userID: null,
+        groupID: null
+      };
       this.instance = TestUtils.renderIntoDocument(
         <SidePanels
           statesProcessed={true}
@@ -55,7 +78,7 @@ describe("SidePanels", function () {
       );
     });
 
-    it("should return false is all IDs are null", function () {
+    it("should return false if all IDs are null", function () {
       expect(this.instance.isOpen()).toEqual(false);
     });
 
@@ -69,7 +92,13 @@ describe("SidePanels", function () {
 
   describe("#getContents", function () {
     beforeEach(function () {
-      this.params = {nodeID: null, serviceName: null, taskID: null};
+      this.params = {
+        nodeID: null,
+        serviceName: null,
+        taskID: null,
+        userID: null,
+        groupID: null
+      };
       this.instance = TestUtils.renderIntoDocument(
         <SidePanels
           statesProcessed={true}
@@ -103,6 +132,24 @@ describe("SidePanels", function () {
       var contents = this.instance.getContents(this.params);
 
       expect(contents.type === ServiceSidePanelContents).toEqual(true);
+      this.params.serviceName = null;
+    });
+
+    it("should return UserSidePanelContents if userID is set",
+      function () {
+      this.params.userID = "set";
+      var contents = this.instance.getContents(this.params);
+
+      expect(contents.type === UserSidePanelContents).toEqual(true);
+      this.params.serviceName = null;
+    });
+
+    it("should return GroupSidePanelContents if groupID is set",
+      function () {
+      this.params.groupID = "set";
+      var contents = this.instance.getContents(this.params);
+
+      expect(contents.type === GroupSidePanelContents).toEqual(true);
       this.params.serviceName = null;
     });
   });
