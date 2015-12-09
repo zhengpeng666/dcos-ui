86a87c24237b69299e0bcb6c8f910443d89a4d62
John Furrow <johnf@mesosphere.io>
2015-12-08 16:37:38 -0800

    Remove ability to edit UID

--- src/js/components/UserDetails.js
+++ src/js/components/UserDetails.js
@@ -9,8 +9,7 @@ import StoreMixin from "../mixins/StoreMixin";
 import Util from "../utils/Util";
 
 const METHODS_TO_BIND = [
-  "handlePasswordSubmit",
-  "handleUsernameSubmit"
+  "handlePasswordSubmit"
 ];
 
 export default class UserGroupTable extends Util.mixin(StoreMixin) {
@@ -77,30 +76,8 @@ export default class UserGroupTable extends Util.mixin(StoreMixin) {
     });
   }
 
-  handleUsernameSubmit(formData) {
-    ACLUserStore.updateUser(this.props.userID, {
-      uid: this.props.userID,
-      password: formData.password
-    });
-  }
-
   render() {
     let userDetails = ACLUserStore.getUser(this.props.userID);
-
-    let usernameDefinition = [
-      {
-        fieldType: "text",
-        name: "uid",
-        placeholder: "Username",
-        required: true,
-        showError: false,
-        showLabel: false,
-        writeType: "edit",
-        validation: function () { return true; },
-        value: userDetails.uid
-      }
-    ];
-
     let passwordDefinition = [
       {
         fieldType: "password",
@@ -127,15 +104,15 @@ export default class UserGroupTable extends Util.mixin(StoreMixin) {
     }
 
     return (
-      <div className="container container-fluid container-pod">
+      <div className="container container-fluid container-pod
+        container-pod-short-top">
         <div className="flex-container-col">
           <dl className="row flex-box">
             <dt className="column-3 emphasize">
               Username
             </dt>
             <dt className="column-9">
-              <Form definition={usernameDefinition}
-                onSubmit={this.handleUsernameSubmit} />
+              {userDetails.uid}
             </dt>
           </dl>
           <dl className="row flex-box">
