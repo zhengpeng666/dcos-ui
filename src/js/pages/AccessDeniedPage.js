18270369f977931b16a983df547878dd7e782f41
Christine Sun <christine.sun@mesosphere.io>
2015-12-10 12:07:10 -0800

    AlertPanel now accepts an icon

--- src/js/pages/AccessDeniedPage.js
+++ src/js/pages/AccessDeniedPage.js
@@ -7,11 +7,11 @@ export default class AccessDeniedPage extends React.Component {
   render() {
     return (
       <AlertPanel
-        title="Access Denied">
-        <i className="icon icon-sprite icon-sprite-jumbo
-          icon-sprite-jumbo-white icon-lost-planet"></i>
+        title="Access Denied"
+        iconClassName="icon icon-sprite icon-sprite-jumbo
+          icon-sprite-jumbo-white icon-lost-planet">
         <p>
-          You do not have access to this service.
+          You do not have access to this service. <br />
           Please contact your DCOS administrator.
         </p>
       </AlertPanel>
