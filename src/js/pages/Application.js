42bde58c386aa2b86d78458fb3c2cf22d49c01f8
Rafael Corral <me@rafaelcorral.com>
2015-12-10 10:05:36 -0800

    spread props to pass 'em down

--- src/js/pages/Application.js
+++ src/js/pages/Application.js
@@ -3,6 +3,6 @@ var RouteHandler = require("react-router").RouteHandler;
 
 export default class Application extends React.Component {
   render() {
-    return <RouteHandler />;
+    return <RouteHandler {...this.props} />;
   }
 }
