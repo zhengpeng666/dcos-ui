5f6b661b88d645abd43a0e239833b1c86ece7989
mlunoe <michael.lunoe@gmail.com>
2015-12-18 15:52:20 -0800

    Adding classList to body in tests

--- jest/setupEnv.js
+++ jest/setupEnv.js
@@ -1,2 +1,7 @@
 // jsdom doesn't have support for localStorage at the moment
 global.localStorage = require("localStorage");
+global.document.body.classList = {
+  add: function () {},
+  remove: function () {},
+  toggle: function () {}
+};
