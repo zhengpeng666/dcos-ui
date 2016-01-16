63e2022274fc4c8cec61a636b22462da4b5ee56b
Christine Sun <christine.sun@mesosphere.io>
2016-01-15 16:05:45 -0800

    other integration tests

--- tests/plugins/settings/SettingsPage-cy.js
+++ tests/plugins/settings/SettingsPage-cy.js
@@ -75,6 +75,87 @@ describe("Settings Page [05k]", function () {
       });
     });
 
+    context("Bulk Actions [04n]", function () {
+
+      beforeEach(function () {
+        cy.configureCluster({
+          mesos: "1-task-healthy",
+          acl: true,
+          plugins: "settings-enabled"
+        })
+        .route(/api\/v1\/groups/, "fx:acl/groups-unicode-truncated")
+        .visitUrl({url: "/settings/organization/groups/", identify: true});
+
+        cy.get("tbody .checkbox").as("checkboxes");
+        cy.get("tbody .checkbox input").as("checkboxesState");
+        cy.get("thead .checkbox").as("checkboxHeader");
+        cy.get("thead .checkbox input").as("checkboxHeaderState");
+      });
+
+      it("sets heading checkbox indeterminate when checkbox toggled [04i]",
+        function () {
+          cy.get("@checkboxes").first().click();
+
+          cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
+            expect($checkboxHeader[0].indeterminate).to.equal(true);
+          });
+        }
+      );
+
+      it("sets heading checkbox checked when all checkboxes toggled [04j]",
+        function () {
+          cy.get("@checkboxes").click({multiple: true});
+
+          cy.get("@checkboxes").should(function ($checkboxes) {
+            expect($checkboxes).length.to.be(4);
+          });
+
+          cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
+            expect($checkboxHeader[0].checked).to.equal(true);
+          });
+        }
+      );
+
+      it("sets heading checkbox indeterminate when checkbox untoggled [04k]",
+        function () {
+          cy.get("@checkboxes").click({multiple: true});
+          cy.get("@checkboxes").first().click();
+
+          cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
+            expect($checkboxHeader[0].indeterminate).to.equal(true);
+          });
+        }
+      );
+
+      it("checks all checkboxes on heading checkbox checked [04l]",
+        function () {
+          cy.get("@checkboxHeader").click();
+
+          cy.get("@checkboxesState").should(function ($checkboxHeader) {
+            expect($checkboxHeader[0].checked).to.equal(true);
+            expect($checkboxHeader[1].checked).to.equal(true);
+            expect($checkboxHeader[2].checked).to.equal(true);
+            expect($checkboxHeader[3].checked).to.equal(true);
+          });
+        }
+      );
+
+      it("unchecks all checkboxes on heading checkbox unchecked [04m]",
+        function () {
+          cy.get("@checkboxHeader").click();
+          cy.get("@checkboxHeader").click();
+
+          cy.get("@checkboxesState").should(function ($checkboxHeader) {
+            expect($checkboxHeader[0].checked).to.equal(false);
+            expect($checkboxHeader[1].checked).to.equal(false);
+            expect($checkboxHeader[2].checked).to.equal(false);
+            expect($checkboxHeader[3].checked).to.equal(false);
+          });
+        }
+      );
+
+    });
+
   });
 
   context("Users Tab [05v]", function () {
