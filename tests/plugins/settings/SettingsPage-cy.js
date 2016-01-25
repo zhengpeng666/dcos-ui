8a3fb6dc5252e0fe86439b261c73b8d5f57018f8
Christine Sun <christine.sun@mesosphere.io>
2016-01-25 15:32:25 -0800

    fix integration tests
    tests kept timing out, seem to be timing out less after partitioned into smaller files.

--- tests/plugins/settings/SettingsPage-cy.js
+++ tests/plugins/settings/SettingsPage-cy.js
@@ -39,7 +39,7 @@ describe('Settings Page [05k]', function () {
       });
     });
 
-    it('displays 'No data' when it has filtered out all groups [05t]', function () {
+    it('displays \'No data\' when it has filtered out all groups [05t]', function () {
       cy.get(".groups-table-header input[type='text']').as('filterTextbox");
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
       cy.get("@tableRows').get('td').as('tableRowCell");
@@ -73,87 +73,6 @@ describe('Settings Page [05k]', function () {
       cy.get("@tableRows").should(function ($tableRows) {
         expect($tableRows.length).to.equal(1);
       });
-    });
-
-    context('Bulk Actions [04n]', function () {
-
-      beforeEach(function () {
-        cy.configureCluster({
-          mesos: '1-task-healthy',
-          acl: true,
-          plugins: 'settings-enabled'
-        })
-        .route(/api\/v1\/groups/, 'fx:acl/groups-unicode-truncated')
-        .visitUrl({url: '/settings/organization/groups/', identify: true});
-
-        cy.get('tbody .checkbox').as('checkboxes');
-        cy.get('tbody .checkbox input').as('checkboxesState');
-        cy.get('thead .checkbox').as('checkboxHeader');
-        cy.get('thead .checkbox input').as('checkboxHeaderState');
-      });
-
-      it('sets heading checkbox indeterminate when checkbox toggled [04i]',
-        function () {
-          cy.get("@checkboxes").first().click();
-
-          cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
-            expect($checkboxHeader[0].indeterminate).to.equal(true);
-          });
-        }
-      );
-
-      it('sets heading checkbox checked when all checkboxes toggled [04j]',
-        function () {
-          cy.get("@checkboxes").click({multiple: true});
-
-          cy.get("@checkboxes").should(function ($checkboxes) {
-            expect($checkboxes).length.to.be(4);
-          });
-
-          cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
-            expect($checkboxHeader[0].checked).to.equal(true);
-          });
-        }
-      );
-
-      it('sets heading checkbox indeterminate when checkbox untoggled [04k]',
-        function () {
-          cy.get("@checkboxes").click({multiple: true});
-          cy.get("@checkboxes").first().click();
-
-          cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
-            expect($checkboxHeader[0].indeterminate).to.equal(true);
-          });
-        }
-      );
-
-      it('checks all checkboxes on heading checkbox checked [04l]',
-        function () {
-          cy.get("@checkboxHeader").click();
-
-          cy.get("@checkboxesState").should(function ($checkboxHeader) {
-            expect($checkboxHeader[0].checked).to.equal(true);
-            expect($checkboxHeader[1].checked).to.equal(true);
-            expect($checkboxHeader[2].checked).to.equal(true);
-            expect($checkboxHeader[3].checked).to.equal(true);
-          });
-        }
-      );
-
-      it('unchecks all checkboxes on heading checkbox unchecked [04m]',
-        function () {
-          cy.get("@checkboxHeader").click();
-          cy.get("@checkboxHeader").click();
-
-          cy.get("@checkboxesState").should(function ($checkboxHeader) {
-            expect($checkboxHeader[0].checked).to.equal(false);
-            expect($checkboxHeader[1].checked).to.equal(false);
-            expect($checkboxHeader[2].checked).to.equal(false);
-            expect($checkboxHeader[3].checked).to.equal(false);
-          });
-        }
-      );
-
     });
 
   });
@@ -197,7 +116,7 @@ describe('Settings Page [05k]', function () {
       });
     });
 
-    it('displays 'No data' when it has filtered out all users [06a]', function () {
+    it('displays \'No data\' when it has filtered out all users [06a]', function () {
       cy.get(".users-table-header input[type='text']').as('filterTextbox");
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
       cy.get("@tableRows').get('td').as('tableRowCell");
@@ -231,148 +150,6 @@ describe('Settings Page [05k]', function () {
       cy.get("@tableRows").should(function ($tableRows) {
         expect($tableRows.length).to.equal(1);
       });
-    });
-
-    context('Bulk Actions [04c]', function () {
-
-      beforeEach(function () {
-        cy.configureCluster({
-          mesos: '1-task-healthy',
-          acl: true,
-          plugins: 'settings-enabled'
-        })
-        .route(/api\/v1\/users/, 'fx:acl/users-unicode-truncated')
-        .visitUrl({url: '/settings/organization/users/', identify: true});
-
-        cy.get('tbody .checkbox').as('checkboxes');
-        cy.get('tbody .checkbox input').as('checkboxesState');
-        cy.get('thead .checkbox').as('checkboxHeader');
-        cy.get('thead .checkbox input').as('checkboxHeaderState');
-      });
-
-      context("Checkboxes [04o]", function () {
-
-        it("sets heading checkbox indeterminate when checkbox toggled [04d]",
-          function () {
-            cy.get("@checkboxes").first().click();
-
-            cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
-              expect($checkboxHeader[0].indeterminate).to.equal(true);
-            });
-          }
-        );
-
-        it("sets heading checkbox checked when all checkboxes toggled [04e]",
-          function () {
-            cy.get("@checkboxes").click({multiple: true});
-
-            cy.get("@checkboxes").should(function ($checkboxes) {
-              expect($checkboxes).length.to.be(4);
-            });
-
-            cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
-              expect($checkboxHeader[0].checked).to.equal(true);
-            });
-          }
-        );
-
-        it("sets heading checkbox indeterminate when checkbox untoggled [04f]",
-          function () {
-            cy.get("@checkboxes").click({multiple: true});
-            cy.get("@checkboxes").first().click();
-
-            cy.get("@checkboxHeaderState").should(function ($checkboxHeader) {
-              expect($checkboxHeader[0].indeterminate).to.equal(true);
-            });
-          }
-        );
-
-        it("checks all checkboxes on heading checkbox checked [04g]",
-          function () {
-            cy.get("@checkboxHeader").click();
-
-            cy.get("@checkboxesState").should(function ($checkboxHeader) {
-              expect($checkboxHeader[0].checked).to.equal(true);
-              expect($checkboxHeader[1].checked).to.equal(true);
-              expect($checkboxHeader[2].checked).to.equal(true);
-              expect($checkboxHeader[3].checked).to.equal(true);
-            });
-          }
-        );
-
-        it("unchecks all checkboxes on heading checkbox unchecked [04h]",
-          function () {
-            cy.get("@checkboxHeader").click();
-            cy.get("@checkboxHeader").click();
-
-            cy.get("@checkboxesState").should(function ($checkboxHeader) {
-              expect($checkboxHeader[0].checked).to.equal(false);
-              expect($checkboxHeader[1].checked).to.equal(false);
-              expect($checkboxHeader[2].checked).to.equal(false);
-              expect($checkboxHeader[3].checked).to.equal(false);
-            });
-          }
-        );
-
-      });
-
-      context("User Actions Dropdown [04p]", function () {
-
-        it("shows dropdown when a checkbox is checked [04q]", function () {
-          cy.get("@checkboxes").first().click();
-
-          cy.get(".dropdown").should(function ($dropdown) {
-            expect($dropdown.length).to.equal(1);
-          });
-        });
-
-        it("doesn't show dropdown when nothing is checked [04r]",
-          function () {
-            cy.get(".dropdown").should(function ($dropdown) {
-              expect($dropdown.length).to.equal(0);
-            });
-          }
-        );
-
-        it("shows correct user actions [04s]", function () {
-
-          cy.get("@checkboxes").first().click();
-          cy.get(".dropdown").click();
-          cy.get(".dropdown .clickable").as("dropdownItems");
-
-          cy.get("@dropdownItems").should(function ($items) {
-            expect($items.length).to.equal(3);
-            expect($items[0].textContent).to.contain("Add");
-          });
-
-        });
-
-      });
-
-      context("User Actions Modal [04v]", function () {
-
-        beforeEach(function () {
-          cy.get("@checkboxes").first().click();
-          cy.get(".dropdown").click();
-          cy.get(".dropdown .clickable").first().click();
-        });
-
-        it("shows the modal when dropdown is clicked [04u]", function () {
-          cy.get("h3").should(function ($title) {
-            expect($title[0].textContent).to.contain("Add");
-          });
-        });
-
-        it("shows validation error when confirm clicked without selection [04w]",
-          function () {
-            cy.get(".modal-footer .button-primary").click();
-            cy.get(".text-error-state").should(function ($error) {
-              expect($error[0].textContent).to.contain("Select from dropdown");
-            });
-          }
-        );
-      });
-
     });
 
   });
