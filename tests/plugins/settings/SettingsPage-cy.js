e18478bb4853ed30cb43416f6412091d0f117c73
Christine Sun <christine.sun@mesosphere.io>
2016-01-25 18:02:30 -0800

    fix mixmatched quotes in tests from quotepocalypse

--- tests/plugins/settings/SettingsPage-cy.js
+++ tests/plugins/settings/SettingsPage-cy.js
@@ -25,52 +25,52 @@ describe('Settings Page [05k]', function () {
 
     it('shows the groups tab as active [05q]', function () {
       cy.get('.page-header-navigation .active').as('activeTab');
-      cy.get("@activeTab').should('contain', 'Groups");
+      cy.get('@activeTab').should('contain', 'Groups');
     });
 
     it('hides groups when no groups match the string [05r]', function () {
-      cy.get(".groups-table-header input[type='text']').as('filterTextbox");
+      cy.get('.groups-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
 
-      cy.get("@filterTextbox').type('foo_bar_baz_qux");
+      cy.get('@filterTextbox').type('foo_bar_baz_qux');
 
-      cy.get("@tableRows").should(function ($tableRows) {
+      cy.get('@tableRows').should(function ($tableRows) {
         expect($tableRows).length.to.be(1);
       });
     });
 
     it('displays \'No data\' when it has filtered out all groups [05t]', function () {
-      cy.get(".groups-table-header input[type='text']').as('filterTextbox");
+      cy.get('.groups-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
-      cy.get("@tableRows').get('td').as('tableRowCell");
+      cy.get('@tableRows').get('td').as('tableRowCell');
 
-      cy.get("@filterTextbox').type('foo_bar_baz_qux");
+      cy.get('@filterTextbox').type('foo_bar_baz_qux');
 
-      cy.get("@tableRowCell").should(function ($tableCell) {
+      cy.get('@tableRowCell').should(function ($tableCell) {
         expect($tableCell[0].textContent).to.equal('No data');
       });
     });
 
     it('shows all groups after clearing the filter [05s]', function () {
-      cy.get(".groups-table-header input[type='text']').as('filterTextbox");
+      cy.get('.groups-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
       cy.get('.groups-table-header .form-control-group-add-on a')
         .as('clearFilterButton');
 
-      cy.get("@filterTextbox').type('foo_bar_baz_qux");
-      cy.get("@clearFilterButton").click();
+      cy.get('@filterTextbox').type('foo_bar_baz_qux');
+      cy.get('@clearFilterButton').click();
 
-      cy.get("@tableRows").should(function ($tableRows) {
+      cy.get('@tableRows').should(function ($tableRows) {
         expect($tableRows).length.to.be.above(10);
       });
     });
 
     it('allows users to filter by unicode characters [05u]', function () {
-      cy.get(".groups-table-header input[type='text']').as('filterTextbox");
+      cy.get('.groups-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
 
-      cy.get("@filterTextbox').type('藍-遙 遥 悠 遼");
-      cy.get("@tableRows").should(function ($tableRows) {
+      cy.get('@filterTextbox').type('藍-遙 遥 悠 遼');
+      cy.get('@tableRows').should(function ($tableRows) {
         expect($tableRows.length).to.equal(1);
       });
     });
@@ -102,52 +102,52 @@ describe('Settings Page [05k]', function () {
 
     it('shows the users tab as active [05y]', function () {
       cy.get('.page-header-navigation .active').as('activeTab');
-      cy.get("@activeTab').should('contain', 'Users");
+      cy.get('@activeTab').should('contain', 'Users');
     });
 
     it('hides users when no users match the string [05z]', function () {
-      cy.get(".users-table-header input[type='text']').as('filterTextbox");
+      cy.get('.users-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
 
-      cy.get("@filterTextbox').type('foo_bar_baz_qux");
+      cy.get('@filterTextbox').type('foo_bar_baz_qux');
 
-      cy.get("@tableRows").should(function ($tableRows) {
+      cy.get('@tableRows').should(function ($tableRows) {
         expect($tableRows).length.to.be(1);
       });
     });
 
     it('displays \'No data\' when it has filtered out all users [06a]', function () {
-      cy.get(".users-table-header input[type='text']').as('filterTextbox");
+      cy.get('.users-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
-      cy.get("@tableRows').get('td').as('tableRowCell");
+      cy.get('@tableRows').get('td').as('tableRowCell');
 
-      cy.get("@filterTextbox').type('foo_bar_baz_qux");
+      cy.get('@filterTextbox').type('foo_bar_baz_qux');
 
-      cy.get("@tableRowCell").should(function ($tableCell) {
+      cy.get('@tableRowCell').should(function ($tableCell) {
         expect($tableCell[0].textContent).to.equal('No data');
       });
     });
 
     it('shows all users after clearing the filter [06b]', function () {
-      cy.get(".users-table-header input[type='text']').as('filterTextbox");
+      cy.get('.users-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
       cy.get('.users-table-header .form-control-group-add-on a')
         .as('clearFilterButton');
 
-      cy.get("@filterTextbox').type('foo_bar_baz_qux");
-      cy.get("@clearFilterButton").click();
+      cy.get('@filterTextbox').type('foo_bar_baz_qux');
+      cy.get('@clearFilterButton').click();
 
-      cy.get("@tableRows").should(function ($tableRows) {
+      cy.get('@tableRows').should(function ($tableRows) {
         expect($tableRows).length.to.be.above(10);
       });
     });
 
     it('allows users to filter by unicode characters [06c]', function () {
-      cy.get(".users-table-header input[type='text']').as('filterTextbox");
+      cy.get('.users-table-header input[type="text"]').as('filterTextbox');
       cy.get('.page-content-fill .table tbody tr').as('tableRows');
 
-      cy.get("@filterTextbox').type('藍-遙 遥 悠 遼");
-      cy.get("@tableRows").should(function ($tableRows) {
+      cy.get('@filterTextbox').type('藍-遙 遥 悠 遼');
+      cy.get('@tableRows').should(function ($tableRows) {
         expect($tableRows.length).to.equal(1);
       });
     });
