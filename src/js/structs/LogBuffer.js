4826c3edcaa78d8e11986a3d58b6f91446fa2320
Kenny Tran <kenny8tran@gmail.com>
2016-01-29 12:03:38 -0800

    Change truncate to work both ways and write tests

--- src/js/structs/LogBuffer.js
+++ src/js/structs/LogBuffer.js
@@ -7,9 +7,18 @@ const PAGE_SIZE = 8 * 4096;  // 32kb of data or 8 'pages'
 const DEFAULT_OPTIONS = {
   end: -1,
   initialized: false,
-  maxFileSize: 50000,
+  maxFileSize: 250000000,
   start: -1
 };
+
+function truncateItemData(itemData, sizeDiff) {
+  // Truncate to fit within maxFileSize
+  itemData = itemData.substring(sizeDiff);
+  // Truncate to first newline
+  let newLineIndex = itemData.indexOf('\n') + 1;
+  return itemData.substring(newLineIndex);
+  // Update currentSize accordingly
+}
 
 export default class LogBuffer extends List {
   constructor(options = {}) {
@@ -49,11 +58,26 @@ export default class LogBuffer extends List {
 
   prepend(entry) {
     let data = entry.get('data');
+    let offset = entry.get('offset');
+
     let start = this.getStart();
+    let end = this.getEnd();
+
+    let index = data.indexOf('\n') + 1;
+    offset += index;
+    data = data.substring(index);
+
+    if (start === end && offset !== 0) {
+      end = offset + data.length;
+    }
+    start = offset;
 
     this.list.unshift(new Item({data, start}));
-    start -= data.length;
     this.configuration.start = start;
+    this.configuration.end = end;
+
+    let fromBack = true;
+    this.truncate(fromBack);
   }
 
   add(entry) {
@@ -76,8 +100,10 @@ export default class LogBuffer extends List {
     // Update end to be offset + new addition to the log
     this.configuration.end = offset + data.length;
     this.configuration.start = start;
+
     // Add log entry
     super.add(new Item({data, offset}));
+    this.truncate();
   }
 
   getEnd() {
@@ -103,45 +129,47 @@ export default class LogBuffer extends List {
    * boundaries given by maxFileSize
    * It will also truncate the data of the 'oldest item to stay in log',
    * to the first newline index
+   * @param {boolean} fromBack Truncate from front or back.
    */
-  truncate() {
-    let end = this.getEnd();
+  truncate(fromBack) {
+    let currentSize = this.getEnd() - this.getStart();
     let maxFileSize = this.configuration.maxFileSize;
+    let sizeDiff = currentSize - maxFileSize;
 
-    if (end - this.getStart() < maxFileSize) {
-      // We are within size, so we don't have to truncate anything
+    if (sizeDiff < 0) {
       return;
     }
 
     let items = this.getItems();
-    let index = items.length - 1;
-    let size = 0;
-    for (; index >= 0; index--) {
-      let item = items[index];
-      let itemData = item.get('data');
-      size += itemData.length;
-
-      if (size > maxFileSize) {
-        let sizeDiff = size - maxFileSize;
-        // Truncate to fit within maxFileSize
-        itemData = itemData.substring(sizeDiff);
-        // Truncate to first newline
-        let newLineIndex = itemData.indexOf('\n') + 1;
-        itemData = itemData.substring(newLineIndex);
-        // Update size accordingly
-        size -= sizeDiff + newLineIndex;
-        items[index] = new Item({
-          data: itemData,
-          offset: item.get('offset')
-        });
-        break;
-      }
+    let index = 0;
+    if (fromBack) {
+      index = items.length - 1;
     }
 
-    // Update start to be the new end minus our file window
-    this.configuration.start = end - size;
-    if (index > 0) {
-      this.list = this.list.slice(index);
+    let item = items[index];
+    let itemData = item.get('data');
+    let originalDatasize = itemData.length;
+
+    itemData = truncateItemData(itemData, sizeDiff);
+    currentSize = currentSize - originalDatasize + itemData.length;
+
+    if (itemData.length > 0) {
+      items[index] = new Item({
+        data: itemData,
+        offset: item.get('offset')
+      });
+    } else {
+      this.list.splice(index, 1);
+    }
+
+    if (fromBack) {
+      this.configuration.end = this.getStart() + currentSize;
+    } else {
+      this.configuration.start = this.getEnd() - currentSize;
+    }
+
+    if (currentSize > maxFileSize) {
+      this.truncate(fromBack);
     }
   }
 }
