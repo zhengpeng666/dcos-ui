6415c1d411d11ac193488418631426963ef28f84
Kenny Tran <kenny8tran@gmail.com>
2016-02-02 10:52:42 -0800

    Integration tests for Universe

---
+++ tests/UniversePage-cy.js
@@ -0,0 +1,19 @@
+describe.only('Universe Page', function () {
+
+  beforeEach(function () {
+    cy.configureCluster({
+      mesos: '1-task-healthy',
+      acl: true
+    })
+    .visitUrl({url: '/'});
+  });
+
+  it('goes to Universe page when tab is clicked', function () {
+    cy.get('.sidebar-menu-item').contains('Universe').click();
+    cy.hash().should('match', /universe/);
+
+    // it('routes to the universe page', function () {
+    // });
+
+  });
+});
