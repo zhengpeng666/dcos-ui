dd6099d33d74921652358f25c9495dec2914e6b5
Kenny Tran <kenny8tran@gmail.com>
2016-02-02 14:59:41 -0800

    Test #throttleScroll

--- src/js/utils/__tests__/Util.js
+++ src/js/utils/__tests__/Util.js
@@ -58,4 +58,33 @@ describe('Util', function () {
       expect(result).toEqual(false);
     });
   });
+
+  describe('#throttleScroll', function () {
+    beforeEach(function () {
+      this.func = jest.genMockFunction();
+      this.throttled = Util.throttleScroll(
+        this.func, 200
+      ).bind(this, {nativeEvent: {}});
+    });
+
+    it('only calls once if called before the wait is finished', function () {
+      this.throttled();
+      this.throttled();
+      this.throttled();
+      this.throttled();
+      expect(this.func.mock.calls.length).toBe(1);
+    });
+
+    it('calls the function if called after the wait', function () {
+      var throttled = this.throttled;
+      var func = this.func;
+
+      throttled();
+      throttled();
+      throttled();
+      jest.runAllTimers();
+
+      expect(func.mock.calls.length).toBe(2);
+    });
+  });
 });
