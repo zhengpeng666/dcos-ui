98d73b49ef3c6e3ba1da8f9da6244ca0e9f19728
Kenny Tran <kenny8tran@gmail.com>
2016-02-02 14:18:15 -0800

    Remove space

--- src/js/pages/UniversePage.js
+++ src/js/pages/UniversePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default class UniversePage extends React.Component {
-
   render() {
     return (
       <h1 className="inverse text-align-center">Dat Cosmos</h1>
