91f1366f8dec4079c7ad7c42b2d2c278e00fd955
mlunoe <michael.lunoe@gmail.com>
2016-02-08 18:50:03 -0800

    Adding describe and list actions

--- src/js/events/CosmosPackagesActions.js
+++ src/js/events/CosmosPackagesActions.js
@@ -5,6 +5,46 @@ import RequestUtil from '../utils/RequestUtil';
 
 const CosmosPackagesActions = {
 
+  describe: function (packageName, packageVersion) {
+    RequestUtil.json({
+      method: 'POST',
+      url: `${Config.rootUrl}${Config.cosmosAPIPrefix}/describe`,
+      data: {packageName, packageVersion},
+      success: function (response) {
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_COSMOS_PACKAGE_DESCRIBE_SUCCESS,
+          data: response
+        });
+      },
+      error: function (xhr) {
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_COSMOS_PACKAGE_DESCRIBE_ERROR,
+          data: RequestUtil.getErrorFromXHR(xhr)
+        });
+      }
+    });
+  },
+
+  list: function (packageName, appId) {
+    RequestUtil.json({
+      method: 'POST',
+      url: `${Config.rootUrl}${Config.cosmosAPIPrefix}/list`,
+      data: {packageName, appId},
+      success: function (response) {
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_COSMOS_PACKAGES_LIST_SUCCESS,
+          data: response.packages
+        });
+      },
+      error: function (xhr) {
+        AppDispatcher.handleServerAction({
+          type: ActionTypes.REQUEST_COSMOS_PACKAGES_LIST_ERROR,
+          data: RequestUtil.getErrorFromXHR(xhr)
+        });
+      }
+    });
+  },
+
   search: function (query) {
     RequestUtil.json({
       method: 'POST',
