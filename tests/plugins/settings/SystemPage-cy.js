6318186a669bd4746485e07952097b4311c1e6d0
Christine Sun <christine.sun@mesosphere.io>
2016-02-08 19:09:17 -0800

    add integration test

--- tests/plugins/settings/SystemPage-cy.js
+++ tests/plugins/settings/SystemPage-cy.js
@@ -10,11 +10,18 @@ describe('System Page [0dw]', function () {
       .visitUrl({url: '/settings/system', identify: true});
     });
 
-    it('goes to Component Health page when Component tab clicked [0dz]', function () {
-      cy.get('.page-header-navigation .tab-item-label')
-        .contains('Components')
-        .click();
-      cy.hash().should('match', /components/);
+    it('goes to Component Health page when Component tab clicked [0dz]',
+      function () {
+        cy.get('.page-header-navigation .tab-item-label')
+          .contains('Components')
+          .click();
+        cy.hash().should('match', /components/);
+      }
+    );
+
+    it('shows the Component Health tab as active [0e1]', function () {
+      cy.get('.page-header-navigation .active').as('activeTab');
+      cy.get('@activeTab').should('contain', 'Components');
     });
 
   });
