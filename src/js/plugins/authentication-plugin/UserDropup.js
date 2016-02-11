f9bc74f123a8a25ca6e4eef42d30fb575d859991
Rafael Corral <me@rafaelcorral.com>
2016-02-11 14:05:31 -0800

    Fix for when all plugins are enabled.

--- src/js/plugins/authentication-plugin/UserDropup.js
+++ src/js/plugins/authentication-plugin/UserDropup.js
@@ -11,6 +11,7 @@ const METHODS_TO_BIND = [
 
 const MENU_ITEMS = {
   'button-docs': 'Documentation',
+  'button-intercom': 'Talk with us',
   'button-tour': 'Install CLI',
   'button-sign-out': 'Sign Out'
 };
