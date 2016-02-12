aec54aecaee6809e51b28ffbeab312ed28a52450
mlunoe <michael.lunoe@gmail.com>
2016-02-12 15:54:06 -0800

    minor perf

--- src/js/events/TaskDirectoryActions.js
+++ src/js/events/TaskDirectoryActions.js
@@ -10,7 +10,7 @@ import RequestUtil from '../utils/RequestUtil';
 function findWithID(stateObject, listProps, id) {
   let idCondition = {id};
   let framework;
-  for (let i = 0; i < listProps.length; i++) {
+  for (let i = 0, len = listProps.length; i < len; i++) {
     framework = _.findWhere(stateObject[listProps[i]], idCondition);
     if (framework) {
       return framework;
