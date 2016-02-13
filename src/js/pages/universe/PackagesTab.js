16ce515d2d7c88da1f94ef9f72d29313682fcfd1
Kenny Tran <kenny8tran@gmail.com>
2016-02-12 16:51:30 -0800

    Fix rebase leftovers

--- src/js/pages/universe/PackagesTab.js
+++ src/js/pages/universe/PackagesTab.js
@@ -1,5 +1,4 @@
 import mixin from 'reactjs-mixin';
-import {Modal} from 'reactjs-components';
 /*eslint-disable no-unused-vars*/
 import React from 'react';
 /*eslint-enable no-unused-vars*/
@@ -7,100 +6,7 @@ import {StoreMixin} from 'mesosphere-shared-reactjs';
 
 import AdvancedConfigModal from '../../components/AdvancedConfigModal';
 import CosmosPackagesStore from '../../stores/CosmosPackagesStore';
-import MultipleForm from '../../components/MultipleForm';
 import Panel from '../../components/Panel';
-import UniversePackagesList from '../../structs/UniversePackagesList';
-
-// TODO (mlunoe): Remove the following mock data!
-let packages = new UniversePackagesList({items: [
-  {
-      'currentVersion': '1.5.0',
-      'description': 'A clust-wide init system and control system for services in cgroups or Docker containers.',
-      'framework': true,
-      'packageName': 'Marathon',
-      'resources': {
-        'images': {
-          'icon-small': 'https://downloads.mesosphere.com/marathon/assets/icon-service-marathon-small.png',
-          'icon-medium': 'https://downloads.mesosphere.com/marathon/assets/icon-service-marathon-medium.png',
-          'icon-large': 'https://downloads.mesosphere.com/marathon/assets/icon-service-marathon-large.png'
-        }
-      },
-      'tags': [
-          'marathon',
-          'dcos',
-          'init',
-          'framework'
-      ],
-      'versions': {
-          '1.5.0': '0'
-      }
-  },
-  {
-      'currentVersion': '0.2.1',
-      'description': 'A distributed free and open-source database with a flexible data model for documents, graphs, and key-values. Build high performance applications using a convenient SQL-like query language or JavaScript extensions.',
-      'framework': true,
-      'packageName': 'arangodb',
-      'resources': {
-        'images': {
-          'icon-small': 'https://downloads.mesosphere.com/marathon/assets/icon-service-marathon-small.png',
-          'icon-medium': 'https://downloads.mesosphere.com/marathon/assets/icon-service-marathon-medium.png',
-          'icon-large': 'https://downloads.mesosphere.com/marathon/assets/icon-service-marathon-large.png'
-        }
-      },
-      'tags': [
-          'arangodb',
-          'NoSQL',
-          'database',
-          'framework'
-      ],
-      'versions': {
-          '0.2.1': '0'
-      }
-  },
-  {
-      'currentVersion': '0.2.0-1',
-      'description': 'Apache Cassandra running on Apache Mesos',
-      'framework': true,
-      'packageName': 'cassandra',
-      'tags': [
-          'data',
-          'database',
-          'nosql'
-      ],
-      'versions': {
-          '0.2.0-1': '0'
-      }
-  },
-  {
-      'currentVersion': '2.4.0',
-      'description': 'A fault tolerant job scheduler for Mesos which handles dependencies and ISO8601based schedules.',
-      'framework': true,
-      'packageName': 'chronos',
-      'tags': [
-          'cron',
-          'analytics',
-          'batch'
-      ],
-      'versions': {
-          '2.4.0': '0'
-      }
-  },
-  {
-      'currentVersion': '0.1.7',
-      'description': 'Hadoop Distributed File System (HDFS), Highly Available',
-      'framework': true,
-      'packageName': 'hdfs',
-      'tags': [
-          'filesystem',
-          'hadoop',
-          'analytics'
-      ],
-      'versions': {
-          '0.1.7': '0'
-      }
-  }
-]});
->>>>>>> Create advanced config modal
 
 const METHODS_TO_BIND = [
   'handleAdvancedModalClose',
@@ -211,7 +117,7 @@ class PackagesTab extends mixin(StoreMixin) {
           Open Advanced Configuration
         </button><br/>
         <AdvancedConfigModal
-          open={this.state.advancedModalOpen}
+          open={advancedModalOpen}
           onClose={this.handleAdvancedModalClose}/>
         {this.getPackages()}
       </div>
