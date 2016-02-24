7464ee9f66acb05b1f07729ec91d5f764df34cbb
mlunoe <michael.lunoe@gmail.com>
2016-02-24 14:57:44 -0800

    Moving findNestedPropertyInObject to Util

--- src/js/structs/UniversePackage.js
+++ src/js/structs/UniversePackage.js
@@ -1,27 +1,18 @@
 import Item from './Item';
 import ServiceUtil from '../utils/ServiceUtil';
-
-function getNestedPropertyInObject(obj = {}, propList = []) {
-  return propList.reduce(function (current, nextProp) {
-    if (!current) {
-      return current;
-    }
-
-    return current[nextProp];
-  }, obj);
-}
+import Util from '../utils/Util';
 
 class UniversePackage extends Item {
   getIcons() {
     return ServiceUtil.getServiceImages(
-      getNestedPropertyInObject(this.get('resource'), ['images'])
+      Util.findNestedPropertyInObject(this.get('resource'), 'images')
     );
   }
 
   getScreenshots() {
-    return getNestedPropertyInObject(
+    return Util.findNestedPropertyInObject(
       this.get('resource'),
-      ['images', 'screenshots']
+      'images.screenshots'
     );
   }
 }
