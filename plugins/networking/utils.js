3c192cac8907b591000267ccd1930ddaa02159bf
Rafael Corral <me@rafaelcorral.com>
2016-03-11 14:55:05 -0800

    Optimizations to logic

--- plugins/networking/utils.js
+++ plugins/networking/utils.js
@@ -19,46 +19,41 @@ let Utils = {
       });
     });
 
-    timestamps.sort();
+    timestamps.reverse();
 
     // For each timestamp, loop through the datasets and create the normalized
     // data array.
-    timestamps.forEach(function (timestamp) {
-      dataSets.forEach(function (dataSet, index) {
+    timestamps.forEach(function (timestamp, timestampIndex) {
+      if (timestampIndex >= MAX_INTERVALS) {
+        return;
+      }
+
+      dataSets.forEach(function (dataSet, dataSetIndex) {
         // Add 0 if the data doesn't exist for that timestamp.
-        normalizedDataSets[index].push(dataSet[timestamp] || 0);
+        normalizedDataSets[dataSetIndex].push(dataSet[timestamp] || 0);
       });
     });
 
     if (timestamps.length === MAX_INTERVALS) {
       // Return now if we have the desired number of intervals.
       return normalizedDataSets;
-    } else if (timestamps.length > MAX_INTERVALS) {
-      // If we have more timestamps than the max intervals, we return the newest
-      // data points we have.
-      normalizedDataSets.forEach(function (dataSet, index) {
-        normalizedDataSets[index] = dataSet.splice(dataSet.length - MAX_INTERVALS);
-      });
-      return normalizedDataSets;
     }
 
     // Check if the most recent timestamp is greater than the interval length.
+    let arrayAction;
     if (timestamps[timestamps.length - 1] > currentTime - INTERVAL_LENGTH) {
       // Need to fill in zeroes at end of array.
-      for (let i = timestamps.length; i < MAX_INTERVALS; i++) {
-        dataSets.forEach(function (dataSet, index) {
-          normalizedDataSets[index].push(0);
-        });
-      }
+      arrayAction = 'push';
     } else {
       // Need to fill in zeroes at beginning of array.
-      for (let i = timestamps.length; i < MAX_INTERVALS; i++) {
-        dataSets.forEach(function (dataSet, index) {
-          normalizedDataSets[index].unshift(0);
-        });
-      }
+      arrayAction = 'unshift';
     }
 
+    for (let i = timestamps.length; i < MAX_INTERVALS; i++) {
+      dataSets.forEach(function (dataSet, dataSetIndex) {
+        normalizedDataSets[dataSetIndex][arrayAction](0);
+      });
+    }
 
     return normalizedDataSets;
   }
