ed4fe81d7b7d31fb3aedcc2b328af724ef013635
mlunoe <michael.lunoe@gmail.com>
2016-03-31 14:23:42 -0700

    using proper english in comment

--- src/js/structs/UsersList.js
+++ src/js/structs/UsersList.js
@@ -4,7 +4,7 @@ import Item from './Item';
 module.exports = class UsersList extends List {
   constructor() {
     super(...arguments);
-    // Replace list items instances of Item.
+    // Replace list items with instances of Item.
     this.list = this.list.map(function (item) {
       if (item instanceof Item) {
         return item;
