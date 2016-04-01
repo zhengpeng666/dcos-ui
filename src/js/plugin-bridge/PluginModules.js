9d4bb339b51ef1a6d6603f76b0b277bb6d553682
Mat Appelman <mat@mesosphere.io>
2016-04-01 16:23:00 -0700

    adding extra util

--- src/js/plugin-bridge/PluginModules.js
+++ src/js/plugin-bridge/PluginModules.js
@@ -14,6 +14,7 @@ module.exports = {
     CompositeState: 'CompositeState'
   },
   utils: {
+    CookieUtils: 'CookieUtils',
     StringUtil: 'StringUtil',
     RequestUtil: 'RequestUtil',
     DOMUtils: 'DOMUtils',
