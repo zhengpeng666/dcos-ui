f483ab2f5a506c62b291cb42ff0cbd0546766e6a
Rafael Corral <me@rafaelcorral.com>
2016-04-08 09:15:57 -0700

    Removed missing intercom things.

--- plugins/oauth/components/UserDropup.js
+++ plugins/oauth/components/UserDropup.js
@@ -13,7 +13,6 @@ const METHODS_TO_BIND = [
 
 const MENU_ITEMS = {
   'button-docs': 'Documentation',
-  'button-intercom': 'Talk with us',
   'button-cli': 'Install CLI',
   'button-sign-out': 'Sign Out'
 };
