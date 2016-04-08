95957c09825c6d86d6e32cd37d32dda8c39c9fe6
John Furrow <johnf@mesosphere.io>
2016-04-08 10:28:31 -0700

    Update DCOS UI loading indicator

--- src/js/pages/ApplicationLoader.js
+++ src/js/pages/ApplicationLoader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import AnimatedLogo from '../components/AnimatedLogo';
+import IconDCOSLogoMark from '../components/icons/IconDCOSLogoMark';
 import {PLUGINS_CONFIGURED} from '../constants/EventTypes';
 import {Hooks} from 'PluginSDK';
 
@@ -32,8 +33,8 @@ export default class ApplicationLoader extends React.Component {
   render() {
     return (
       <div id="canvas">
-        <div className="container container-pod vertical-center">
-          <AnimatedLogo speed={500} scale={0.16} />
+        <div className="application-loading-indicator container container-pod vertical-center">
+          <IconDCOSLogoMark />
         </div>
       </div>
     );
