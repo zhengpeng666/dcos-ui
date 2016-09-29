be5831cc4cd2469d37306b11128fb3f9ca0bdbcf
John Furrow <johnf@mesosphere.io>
2016-09-28 17:02:12 -0700

    Monkey-patch requestAnimationFrame

--- jest/setupEnv.js
+++ jest/setupEnv.js
@@ -3,6 +3,12 @@ require('babel-polyfill');
 // jsdom doesn't have support for localStorage at the moment
 global.localStorage = require('localStorage');
 
+// jsdom doesn't have support for requestAnimationFrame so we just make it work.
+global.requestAnimationFrame = function (func) {
+  var args = Array.prototype.slice.call(arguments).slice(1);
+  return func.apply(this, args);
+};
+
 // Tests should just mock responses for the json API
 // so let's just default to a noop
 var RequestUtil = require('mesosphere-shared-reactjs').RequestUtil;
