12e677a72a7450a61eeb0b4ac5c7bd266f175eaf
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2016-11-11 11:15:39 +0100

    Remove inversion of control from Sidebar#getGroupSubmenu

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -127,13 +127,13 @@ var Sidebar = React.createClass({
 
   getNavigationGroup(group, currentPath) {
     let groupMenuItems = group.children.map((element, index) => {
+      let submenu;
+      let isChildActive = false;
       let hasChildren = element.children && element.children.length !== 0;
-
       let isParentActive = currentPath.startsWith(element.path);
-      let {isChildActive, submenu} = this.getGroupSubmenu(element, {
-        currentPath,
-        isParentActive
-      });
+      if (isParentActive) {
+        [submenu, isChildActive] = this.getGroupSubmenu(element, currentPath);
+      }
 
       let linkElement = element.link;
       if (typeof linkElement === 'string') {
@@ -163,40 +163,38 @@ var Sidebar = React.createClass({
     return <ul className="sidebar-menu">{groupMenuItems}</ul>;
   },
 
-  getGroupSubmenu({path, children = []}, {currentPath, isParentActive}) {
+  getGroupSubmenu({path, children = []}, currentPath) {
     let submenu = null;
     let isChildActive = false;
 
-    if (isParentActive) {
-      let menuItems = children.reduce(function (children, currentChild) {
-        let isActive = currentPath.startsWith(currentChild.path);
+    let menuItems = children.reduce(function (children, currentChild) {
+      let isActive = currentPath.startsWith(currentChild.path);
 
-        let menuItemClasses = classNames({selected: isActive});
+      let menuItemClasses = classNames({selected: isActive});
 
-        if (!isChildActive && isActive) {
-          isChildActive = true;
-        }
-
-        let linkElement = currentChild.link;
-        if (typeof linkElement === 'string') {
-          linkElement = <Link to={currentChild.path}>{linkElement}</Link>;
-        }
-
-        children.push(
-          <li className={menuItemClasses} key={currentChild.label}>
-            {linkElement}
-          </li>
-        );
-
-        return children;
-      }, []);
-
-      if (menuItems.length) {
-        submenu = <ul>{menuItems}</ul>;
+      if (!isChildActive && isActive) {
+        isChildActive = true;
       }
+
+      let linkElement = currentChild.link;
+      if (typeof linkElement === 'string') {
+        linkElement = <Link to={currentChild.path}>{linkElement}</Link>;
+      }
+
+      children.push(
+        <li className={menuItemClasses} key={currentChild.label}>
+          {linkElement}
+        </li>
+      );
+
+      return children;
+    }, []);
+
+    if (menuItems.length) {
+      submenu = <ul>{menuItems}</ul>;
     }
 
-    return {submenu, isChildActive};
+    return [submenu, isChildActive];
   },
 
   getVersion() {
